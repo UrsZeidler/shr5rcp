2722cdecc90aaf767e5b89721e48718face298d6
UrsZeidler <github@urszeidler.de>
2014-01-28 23:37:25 +0100

    added an widget for the skills

---
+++ de.urszeidler.shr5.ecp/src/de/urszeidler/shr5/ecp/editor/widgets/PersonaFertigkeitenWidget.java
@@ -0,0 +1,392 @@
+package de.urszeidler.shr5.ecp.editor.widgets;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.core.databinding.DataBindingContext;
+import org.eclipse.core.databinding.observable.list.IObservableList;
+import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.databinding.edit.EMFEditObservables;
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.edit.command.SetCommand;
+import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
+import org.eclipse.emf.edit.ui.provider.ExtendedImageRegistry;
+import org.eclipse.jface.layout.TreeColumnLayout;
+import org.eclipse.jface.viewers.CellEditor;
+import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.jface.viewers.ColumnPixelData;
+import org.eclipse.jface.viewers.EditingSupport;
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.TreeViewerColumn;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Spinner;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeColumn;
+import org.eclipse.ui.forms.widgets.FormToolkit;
+
+import de.urszeidler.eclipse.shr5.AbstraktPersona;
+import de.urszeidler.eclipse.shr5.Fertigkeit;
+import de.urszeidler.eclipse.shr5.FertigkeitsGruppe;
+import de.urszeidler.eclipse.shr5.PersonaFertigkeit;
+import de.urszeidler.eclipse.shr5.PersonaFertigkeitsGruppe;
+import de.urszeidler.eclipse.shr5.Shr5Factory;
+import de.urszeidler.eclipse.shr5.Shr5Package;
+import de.urszeidler.eclipse.shr5.Shr5Package.Literals;
+import de.urszeidler.eclipse.shr5.Sprachfertigkeit;
+import de.urszeidler.eclipse.shr5.Wissensfertigkeit;
+import de.urszeidler.eclipse.shr5.util.AdapterFactoryUtil;
+import de.urszeidler.eclipse.shr5.util.ShadowrunTools;
+
+/**
+ * 
+ */
+public class PersonaFertigkeitenWidget extends Composite {
+
+    /**
+     * 
+     */
+    public class SpinnerCellEditor extends CellEditor {
+
+        public SpinnerCellEditor(Composite parent, int style) {
+            super(parent, style);
+        }
+
+        public SpinnerCellEditor(Composite parent) {
+            super(parent);
+        }
+
+        @Override
+        protected Control createControl(Composite parent) {
+            Spinner spinner = new Spinner(parent, SWT.NONE);
+            return spinner;
+        }
+
+        @Override
+        protected Object doGetValue() {
+            return getControl().getSelection();
+        }
+
+        @Override
+        protected void doSetFocus() {
+            getControl().setFocus();
+        }
+
+        @Override
+        public Spinner getControl() {
+            return (Spinner)super.getControl();
+        }
+
+        @Override
+        protected void doSetValue(Object value) {
+            getControl().setSelection((Integer)value);
+        }
+    }
+
+    /**
+     * 
+     */
+    public class GroupWrapper {
+        private List<Fertigkeit> entries = new ArrayList<Fertigkeit>();
+        private String name;
+    }
+
+    /**
+     * @author urs
+     */
+    public class FertigkeitsContentProvider implements ITreeContentProvider {
+        private Object[] emtpty = {};
+
+        @Override
+        public void dispose() {
+        }
+
+        @Override
+        public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+        }
+
+        @Override
+        public Object[] getElements(Object inputElement) {
+            return getChildren(inputElement);
+        }
+
+        @Override
+        public Object[] getChildren(Object parentElement) {
+            if (parentElement instanceof FertigkeitsGruppe) {
+                FertigkeitsGruppe fg = (FertigkeitsGruppe)parentElement;
+                return fg.getFertigkeiten().toArray();
+            } else if (parentElement instanceof GroupWrapper) {
+                GroupWrapper gw = (GroupWrapper)parentElement;
+                return gw.entries.toArray();
+            } else if (parentElement instanceof Collection) {
+                Collection<?> c = (Collection<?>)parentElement;
+                return c.toArray();
+            }
+            return emtpty;
+        }
+
+        @Override
+        public Object getParent(Object element) {
+            return null;
+        }
+
+        @Override
+        public boolean hasChildren(Object element) {
+            return getChildren(element).length > 0;
+        }
+    }
+
+    private DataBindingContext m_bindingContext;
+
+    private FormToolkit toolkit;// = new FormToolkit(Display.getCurrent());
+    private AbstraktPersona persona;
+    private EditingDomain editingDomain;
+    private TreeViewer treeViewer;
+    private Composite composite;
+
+    private IObservableList personaFertigkeiten;
+
+    private IObservableList personaFertigkeitsGruppen;
+
+    /**
+     * Create the composite.
+     * 
+     * @param parent
+     * @param style
+     */
+    public PersonaFertigkeitenWidget(Composite parent, int style) {
+        super(parent, style);
+        toolkit = new FormToolkit(Display.getCurrent());
+        addDisposeListener(new DisposeListener() {
+            public void widgetDisposed(DisposeEvent e) {
+                toolkit.dispose();
+            }
+        });
+        createWidgets();
+    }
+
+    public PersonaFertigkeitenWidget(Composite parent, int style, AbstraktPersona object, FormToolkit toolkit2, EditingDomain ed) {
+        super(parent, style);
+        toolkit = toolkit2;
+        persona = object;
+        editingDomain = ed;
+
+        createWidgets();
+    }
+
+    protected List<Object> createFertigkeitGroupsRoot() {
+        Collection<EObject> groups = ItemPropertyDescriptor.getReachableObjectsOfType(persona, Shr5Package.Literals.FERTIGKEITS_GRUPPE);
+        Collection<EObject> skill = ItemPropertyDescriptor.getReachableObjectsOfType(persona, Shr5Package.Literals.FERTIGKEIT);
+
+        GroupWrapper ungrouped = new GroupWrapper();
+        ungrouped.name = "Ungrouped skills";
+        GroupWrapper knownGroup = new GroupWrapper();
+        knownGroup.name = "Knowlege skills";
+        GroupWrapper languGoup = new GroupWrapper();
+        languGoup.name = "Language skills";
+
+        for (EObject eObject : skill) {
+            if (eObject instanceof Sprachfertigkeit) {
+                Sprachfertigkeit wf = (Sprachfertigkeit)eObject;
+                languGoup.entries.add(wf);
+            } else if (eObject instanceof Wissensfertigkeit) {
+                Wissensfertigkeit wf = (Wissensfertigkeit)eObject;
+                knownGroup.entries.add(wf);
+            } else if (eObject instanceof Fertigkeit) {
+                Fertigkeit f = (Fertigkeit)eObject;
+                if (!Shr5Package.Literals.FERTIGKEITS_GRUPPE__FERTIGKEITEN.equals(f.eContainingFeature())) {
+                    ungrouped.entries.add(f);
+                }
+            }
+        }
+
+        List<Object> list = new ArrayList<Object>();
+        list.addAll(groups);
+        list.add(ungrouped);
+        list.add(knownGroup);
+        list.add(languGoup);
+
+        return list;
+    }
+
+    private void createWidgets() {
+        toolkit.adapt(this);
+        toolkit.paintBordersFor(this);
+        setLayout(new FillLayout(SWT.HORIZONTAL));
+        personaFertigkeiten = EMFEditObservables.observeList(editingDomain, persona, Literals.ABSTRAKT_PERSONA__FERTIGKEITEN);
+        personaFertigkeitsGruppen = EMFEditObservables.observeList(editingDomain, persona, Literals.ABSTRAKT_PERSONA__FERTIGKEITS_GRUPPEN);
+
+        composite = new Composite(this, SWT.NONE);
+        toolkit.adapt(composite);
+        toolkit.paintBordersFor(composite);
+        TreeColumnLayout tcl_composite = new TreeColumnLayout();
+        composite.setLayout(tcl_composite);
+
+        treeViewer = new TreeViewer(composite, SWT.BORDER);
+        treeViewer.setAutoExpandLevel(2);
+        final Tree tree = treeViewer.getTree();
+        tree.setHeaderVisible(true);
+        tree.setLinesVisible(true);
+        toolkit.paintBordersFor(tree);
+
+        TreeViewerColumn treeViewerNameColumn = new TreeViewerColumn(treeViewer, SWT.NONE);
+        treeViewerNameColumn.setLabelProvider(new ColumnLabelProvider() {
+            public Image getImage(Object object) {
+                return ExtendedImageRegistry.getInstance().getImage(AdapterFactoryUtil.getInstance().getItemDelegator().getImage(object));
+            }
+
+            public String getText(Object object) {
+                if (object instanceof GroupWrapper) {
+                    GroupWrapper gw = (GroupWrapper)object;
+                    return gw.name;
+                }
+
+                return AdapterFactoryUtil.getInstance().getItemDelegator().getText(object);
+            }
+        });
+        TreeColumn trclmnName = treeViewerNameColumn.getColumn();
+        tcl_composite.setColumnData(trclmnName, new ColumnPixelData(300, true, true));
+        trclmnName.setText("Gruppen/Fertigkeiten");
+
+        TreeViewerColumn treeViewerValueColumn = new TreeViewerColumn(treeViewer, SWT.NONE);
+        treeViewerValueColumn.setLabelProvider(new ColumnLabelProvider() {
+            public Image getImage(Object object) {
+                return null;
+            }
+
+            public String getText(Object element) {
+                if (element instanceof Fertigkeit) {
+                    Fertigkeit f = (Fertigkeit)element;
+                    Integer fertigkeitValue = ShadowrunTools.findFertigkeitValue(f, persona);
+                    return fertigkeitValue.toString();
+
+                } else if (element instanceof FertigkeitsGruppe) {
+                    FertigkeitsGruppe fg = (FertigkeitsGruppe)element;
+                    return ShadowrunTools.findFertigkeitValue(fg, persona) + "";
+                } else if (element instanceof GroupWrapper) {
+                    return "";
+                }
+                return element == null ? "" : element.toString();
+            }
+        });
+        treeViewerValueColumn.setEditingSupport(new EditingSupport(treeViewer) {
+            protected boolean canEdit(Object element) {
+                if (element instanceof GroupWrapper) {
+                    return false;
+                }
+                return true;
+            }
+
+            protected CellEditor getCellEditor(Object element) {
+                if (element instanceof GroupWrapper) {
+                    return null;
+                }
+
+                SpinnerCellEditor spinnerCellEditor = new SpinnerCellEditor(tree);
+                return spinnerCellEditor;
+            }
+
+            protected Object getValue(Object element) {
+                if (element instanceof Fertigkeit) {
+                    Fertigkeit f = (Fertigkeit)element;
+                    return ShadowrunTools.findFertigkeitValue(f, persona);
+                } else if (element instanceof FertigkeitsGruppe) {
+                    FertigkeitsGruppe fg = (FertigkeitsGruppe)element;
+                    return ShadowrunTools.findFertigkeitValue(fg, persona);
+                }
+                return null;
+            }
+
+            protected void setValue(Object element, Object value) {
+                if (element instanceof Fertigkeit) {
+                    Fertigkeit f = (Fertigkeit)element;
+                    PersonaFertigkeit personaFertigkeit = ShadowrunTools.findFertigkeit(f, persona);
+                    if (personaFertigkeit == null) {
+                        PersonaFertigkeit pf = Shr5Factory.eINSTANCE.createPersonaFertigkeit();
+                        pf.setFertigkeit(f);
+                        pf.setStufe((Integer)value);
+                        personaFertigkeiten.add(pf);
+                    } else {
+                        Command cmd = SetCommand.create(editingDomain, personaFertigkeit, Shr5Package.Literals.PERSONA_FERTIGKEIT__STUFE, value);
+                        editingDomain.getCommandStack().execute(cmd);
+                    }
+                } else if (element instanceof FertigkeitsGruppe) {
+                    FertigkeitsGruppe fg = (FertigkeitsGruppe)element;
+                    PersonaFertigkeitsGruppe personaFertigkeitsGruppe = ShadowrunTools.findGruppe(fg, persona);
+                    if (personaFertigkeitsGruppe == null) {
+                        PersonaFertigkeitsGruppe pfg = Shr5Factory.eINSTANCE.createPersonaFertigkeitsGruppe();
+                        pfg.setGruppe(fg);
+                        pfg.setStufe((Integer)value);
+                        personaFertigkeitsGruppen.add(pfg);
+                    } else {
+                        Command cmd = SetCommand.create(editingDomain, personaFertigkeitsGruppe,
+                                Shr5Package.Literals.PERSONA_FERTIGKEITS_GRUPPE__STUFE, value);
+                        editingDomain.getCommandStack().execute(cmd);
+                    }
+                }
+                treeViewer.refresh(true);
+            }
+        });
+        TreeColumn trclmnValue = treeViewerValueColumn.getColumn();
+        tcl_composite.setColumnData(trclmnValue, new ColumnPixelData(70, true, true));
+        trclmnValue.setText("Skill value");
+
+        TreeViewerColumn treeViewerTotalColumn = new TreeViewerColumn(treeViewer, SWT.NONE);
+        treeViewerTotalColumn.setLabelProvider(new ColumnLabelProvider() {
+            public Image getImage(Object element) {
+                return null;
+            }
+
+            public String getText(Object element) {
+                if (element instanceof Fertigkeit) {
+                    Fertigkeit f = (Fertigkeit)element;
+                    EAttribute attribut = f.getAttribut();
+                    if (attribut != null) {
+                        Integer value = (Integer)persona.eGet(attribut);
+                        String attributeText = AdapterFactoryUtil.getInstance().getItemDelegator().getText(attribut);
+                        Integer fertigkeitValue = ShadowrunTools.findFertigkeitValue(f, persona);
+                        if (fertigkeitValue < 1 && !f.isAusweichen())
+                            return "----";
+                        value = value + fertigkeitValue;
+                        return "[" + attributeText + "] : " + value.toString();
+                    }
+                } else if (element instanceof FertigkeitsGruppe) {
+                    FertigkeitsGruppe fg = (FertigkeitsGruppe)element;
+                    return ShadowrunTools.findFertigkeitValue(fg, persona) + "";
+                } else if (element instanceof GroupWrapper) {
+                    return "";
+
+                }
+                return element == null ? "" : element.toString();
+            }
+        });
+        TreeColumn trclmnTotalColumn = treeViewerTotalColumn.getColumn();
+        tcl_composite.setColumnData(trclmnTotalColumn, new ColumnPixelData(70, true, true));
+        trclmnTotalColumn.setText("dice pool");
+
+        treeViewer.setContentProvider(new FertigkeitsContentProvider());
+
+        treeViewer.setInput(createFertigkeitGroupsRoot());
+        m_bindingContext = initDataBindings();
+
+    }
+
+    protected DataBindingContext initDataBindings() {
+        DataBindingContext bindingContext = new DataBindingContext();
+        //
+
+        //
+        return bindingContext;
+    }
+}
