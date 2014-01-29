5848cd2e1c2620036834f98da0676df196bdd99c
UrsZeidler <github@urszeidler.de>
2014-01-29 21:16:42 +0100

    added creation for default workspace

---
+++ de.urszeidler.shr5.ecp/src/de/urszeidler/shr5/ecp/Shr5Initalizer.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package de.urszeidler.shr5.ecp;
+
+import org.eclipse.emf.ecp.core.exceptions.ECPProjectWithNameExistsException;
+import org.eclipse.ui.IStartup;
+
+/**
+ * @author urs
+ */
+public class Shr5Initalizer implements IStartup {
+
+    /*
+     * (non-Javadoc)
+     * @see org.eclipse.ui.IStartup#earlyStartup()
+     */
+    @Override
+    public void earlyStartup() {
+
+        Activator.getDefault().createDefaultWorkspace();
+        try {
+            Activator.getDefault().createECPWorkspace();
+        } catch (ECPProjectWithNameExistsException e) {
+            Activator.logError("Error while createing default workspace.", e);
+        }
+
+    }
+
+}
