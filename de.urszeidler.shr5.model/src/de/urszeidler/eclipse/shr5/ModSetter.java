2207ba94fff76f12b278806130363e282c3dcc20
UrsZeidler <github@urszeidler.de>
2014-02-01 15:28:47 +0100

    added acces to the map

--- de.urszeidler.shr5.model/src/de/urszeidler/eclipse/shr5/ModSetter.java
+++ de.urszeidler.shr5.model/src/de/urszeidler/eclipse/shr5/ModSetter.java
@@ -5,6 +5,7 @@ package de.urszeidler.eclipse.shr5;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,172 +22,163 @@ import org.eclipse.emf.ecore.impl.ENotificationImpl;
 
 /**
  * @author urs
- * 
  */
 public class ModSetter implements Adapter {
-	private Map<EAttribute, Value> fMap = new HashMap<EAttribute, Value>();
-	private AbstraktPersona persona;
-	private boolean dirty = true;
+    private Map<EAttribute, Integer> fMap = new HashMap<EAttribute, Integer>();
+    private AbstraktPersona persona;
+    private boolean dirty = true;
 
-	/**
-	 * @author urs
-	 * 
-	 */
-	private class Value {
-		int modWert = 0;
-	}
+    @SuppressWarnings({ "unchecked" })
+    public void setModvalues() {
+        List<Modifizierbar> list = new ArrayList<Modifizierbar>(); // persona.getModifikatoren();
+        // FeatureMap featureMap = persona.getMods();
+        EList<EReference> contents = persona.eClass().getEAllReferences();
+        for (EReference eref : contents) {
+            if (!isExcluded(eref))
+                if (eref.isMany()) {
+                    Object ob = persona.eGet(eref);
+                    if (ob instanceof List) {
+                        List<Modifizierbar> list1 = (List<Modifizierbar>)ob;
+                        if (list1.size() > 0)
+                            if (list1.get(0) instanceof Modifizierbar) {
+                                list.addAll(list1);
+                            }
+                    }
+                }
+        }
 
-	// private static ModSetter instance;
-	//
-	// public static Object getInstance() {
-	// if (instance == null)
-	// instance = new ModSetter();
-	//
-	// return instance;
-	// }
+        fMap.clear();
+        dirty = false;
+        for (Modifizierbar modifikatoren : list) {
+            addModificationValue(modifikatoren);
+        }
 
-	@SuppressWarnings({ "unchecked" })
-	public void setModvalues() {
-		List<Modifizierbar> list = new ArrayList<Modifizierbar>(); // persona.getModifikatoren();
-		// FeatureMap featureMap = persona.getMods();
-		EList<EReference> contents = persona.eClass().getEAllReferences();
-		for (EReference eref : contents) {
-			if (!isExcluded(eref))
-				if (eref.isMany()) {
-					Object ob = persona.eGet(eref);
-					if (ob instanceof List) {
-						List<Modifizierbar> list1 = (List<Modifizierbar>) ob;
-						if (list1.size() > 0)
-							if (list1.get(0) instanceof Modifizierbar) {
-								list.addAll(list1);
-							}
-					}
-				}
-		}
+    }
 
-		fMap.clear();
-		dirty = false;
-		for (Modifizierbar modifikatoren : list) {
-			addModificationValue(modifikatoren);
-		}
+    private boolean isExcluded(EReference eref) {
+        return eref.equals(Shr5Package.Literals.ABSTRAKT_PERSONA__FERTIGKEITEN)
+                || eref.equals(Shr5Package.Literals.ABSTRAKT_PERSONA__FERTIGKEITS_GRUPPEN);
+    }
 
-	}
+    private void addModificationValue(Modifizierbar modifikatoren) {
+        EList<AttributModifikatorWert> mods = modifikatoren.getMods();
+        for (AttributModifikatorWert wert : mods) {
+            if (wert.getAttribut() != null) {
+                Integer value = fMap.get(wert.getAttribut());
+                if (value == null) {
+                    value = new Integer(0);
+                    fMap.put(wert.getAttribut(), value);
+                }
+                value = value + wert.getWert();
+            }
+        }
+    }
 
-	private boolean isExcluded(EReference eref) {
-		return eref.equals(Shr5Package.Literals.ABSTRAKT_PERSONA__FERTIGKEITEN)
-				|| eref.equals(Shr5Package.Literals.ABSTRAKT_PERSONA__FERTIGKEITS_GRUPPEN);
-	}
+    public int getmodWert(EAttribute feature) {
+        if (dirty)
+            setModvalues();
 
-	private void addModificationValue(Modifizierbar modifikatoren) {
-		EList<AttributModifikatorWert> mods = modifikatoren.getMods();
-		for (AttributModifikatorWert wert : mods) {
-			if (wert.getAttribut() != null) {
-				Value value = fMap.get(wert.getAttribut());
-				if (value == null) {
-					value = new Value();
-					fMap.put(wert.getAttribut(), value);
-				}
-				value.modWert = value.modWert + wert.getWert();
-			}
-		}
-	}
+        Integer value = fMap.get(feature);
+        // Set<EAttribute> keySet = fMap.keySet();
 
-	public int getmodWert(EAttribute feature) {
-		if (dirty)
-			setModvalues();
+        int ret;
+        if (value == null)
+            ret = 0;
+        else
+            ret = value;
 
-		Value value = fMap.get(feature);
-		// Set<EAttribute> keySet = fMap.keySet();
+        return ret;
+    }
 
-		int ret;
-		if (value == null)
-			ret = 0;
-		else
-			ret = value.modWert;
+    public AbstraktPersona getPersona() {
+        return persona;
+    }
 
-		return ret;
-	}
+    public void setPersona(AbstraktPersona persona) {
+        this.persona = persona;
+        this.persona.eAdapters().add(this);
+    }
 
-	public AbstraktPersona getPersona() {
-		return persona;
-	}
+    public boolean isDirty() {
+        return dirty;
+    }
 
-	public void setPersona(AbstraktPersona persona) {
-		this.persona = persona;
-		this.persona.eAdapters().add(this);
-	}
+    public void setDirty(boolean dirty) {
+        this.dirty = dirty;
+    }
 
-	public boolean isDirty() {
-		return dirty;
-	}
+    /**
+     * Checks if the changes affects the modsetter.
+     */
+    @Override
+    public void notifyChanged(Notification notification) {
+        // System.out.println("notifcation :" + notification);
 
-	public void setDirty(boolean dirty) {
-		this.dirty = dirty;
-	}
+        Object object = notification.getFeature();
+        if (object instanceof EReference) {
+            EReference eRef = (EReference)object;
 
-	/**
-	 * Checks if the changes affects the modsetter.
-	 */
-	@Override
-	public void notifyChanged(Notification notification) {
-		//System.out.println("notifcation :" + notification);
+            EClass eReferenceType = eRef.getEReferenceType();
+            if (Shr5Package.Literals.MODIFIZIERBAR.isSuperTypeOf(eReferenceType))
+                notifyModChanges(notification);
+        }
+    }
 
-		Object object = notification.getFeature();
-		if (object instanceof EReference) {
-			EReference eRef = (EReference) object;
+    /**
+     * Calls for all mod the notification.
+     * 
+     * @param notification
+     */
+    @SuppressWarnings("unchecked")
+    private void notifyModChanges(Notification notification) {
+        setDirty(true);
 
-			EClass eReferenceType = eRef.getEReferenceType();
-			if (Shr5Package.Literals.MODIFIZIERBAR.isSuperTypeOf(eReferenceType))
-				notifyModChanges(notification);
-		}
-	}
+        List<AttributModifikatorWert> mods = null;
+        if (notification.getEventType() == Notification.REMOVE_MANY || (notification.getEventType() == Notification.ADD_MANY)) {
+            Collection<Modifizierbar> newValue = (Collection<Modifizierbar>)notification.getNewValue();
+            if (newValue == null)
+                newValue = (Collection<Modifizierbar>)notification.getOldValue();
 
-	/**
-	 * Calls for all mod the notification.
-	 * 
-	 * @param notification
-	 */
-	@SuppressWarnings("unchecked")
-	private void notifyModChanges(Notification notification) {
-		setDirty(true);
+            mods = new ArrayList<AttributModifikatorWert>();
+            for (Modifizierbar modifizierbar : newValue) {
+                mods.addAll(modifizierbar.getMods());
+            }
 
-		List<AttributModifikatorWert> mods = null;
-		if (notification.getEventType() == Notification.REMOVE_MANY || (notification.getEventType() == Notification.ADD_MANY)) {
-			Collection<Modifizierbar> newValue = (Collection<Modifizierbar>) notification.getNewValue();
-			if (newValue == null)
-				newValue = (Collection<Modifizierbar>) notification.getOldValue();
+        } else {
+            Modifizierbar newValue = (Modifizierbar)notification.getNewValue();
+            if (newValue == null)
+                newValue = (Modifizierbar)notification.getOldValue();
 
-			mods = new ArrayList<AttributModifikatorWert>();
-			for (Modifizierbar modifizierbar : newValue) {
-				mods.addAll(modifizierbar.getMods());
-			}
+            mods = newValue.getMods();
+        }
+        for (AttributModifikatorWert attributModifikatorWert : mods) {
+            EAttribute attribut = attributModifikatorWert.getAttribut();
+            if (attribut != null)
+                persona.eNotify(new ENotificationImpl((InternalEObject)persona, Notification.SET, attribut, 0, 1));
+        }
+    }
 
-		} else {
-			Modifizierbar newValue = (Modifizierbar) notification.getNewValue();
-			if (newValue == null)
-				newValue = (Modifizierbar) notification.getOldValue();
+    @Override
+    public Notifier getTarget() {
+        return persona;
+    }
 
-			mods = newValue.getMods();
-		}
-		for (AttributModifikatorWert attributModifikatorWert : mods) {
-			EAttribute attribut = attributModifikatorWert.getAttribut();
-			if (attribut != null)
-				persona.eNotify(new ENotificationImpl((InternalEObject) persona, Notification.SET, attribut, 0, 1));
-		}
-	}
+    @Override
+    public void setTarget(Notifier newTarget) {
 
-	@Override
-	public Notifier getTarget() {
-		return persona;
-	}
+    }
 
-	@Override
-	public void setTarget(Notifier newTarget) {
+    @Override
+    public boolean isAdapterForType(Object type) {
+        return false;
+    }
 
-	}
-
-	@Override
-	public boolean isAdapterForType(Object type) {
-		return false;
-	}
+    /**
+     * A read only map from the active modificators.
+     * 
+     * @return
+     */
+    public Map<EAttribute, Integer> getModificatorMap() {
+        return Collections.unmodifiableMap(fMap);
+    }
 }
