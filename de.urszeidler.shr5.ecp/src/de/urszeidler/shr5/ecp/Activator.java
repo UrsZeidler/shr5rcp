9a2405683526d065ae23a1c7c2e6f55b1d24a818
UrsZeidler <github@urszeidler.de>
2014-02-05 23:02:53 +0100

    removed commended code

--- de.urszeidler.shr5.ecp/src/de/urszeidler/shr5/ecp/Activator.java
+++ de.urszeidler.shr5.ecp/src/de/urszeidler/shr5/ecp/Activator.java
@@ -59,10 +59,6 @@ public class Activator extends AbstractUIPlugin {
     public void start(BundleContext context) throws Exception {
         super.start(context);
         plugin = this;
-        
-        //createDefaultWorkspace();
-        
-       // createECPWorkspace();
 
     }
 
