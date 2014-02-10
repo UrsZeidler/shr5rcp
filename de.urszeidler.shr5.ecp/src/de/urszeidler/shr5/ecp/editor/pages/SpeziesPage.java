df3af92f96df6417489b44f48767819d3578079e
UrsZeidler <github@urszeidler.de>
2014-02-10 08:27:19 +0100

    delegate to the displayed feature names

--- de.urszeidler.shr5.ecp/src/de/urszeidler/shr5/ecp/editor/pages/SpeziesPage.java
+++ de.urszeidler.shr5.ecp/src/de/urszeidler/shr5/ecp/editor/pages/SpeziesPage.java
@@ -21,168 +21,156 @@ import de.urszeidler.shr5.ecp.editor.widgets.BeschreibbarWidget;
 
 /**
  * This pages handles {@link Spezies}.
+ * 
  * @author urs
- *
  */
 public class SpeziesPage extends AbstractShr5Page<Spezies> {
-	private Spezies object;
-	private EditingDomain editingDomain;
+    private Spezies object;
+    private EditingDomain editingDomain;
 
-	private DataBindingContext m_bindingContext;
+    private DataBindingContext m_bindingContext;
 
-	/**
-	 * Create the form page.
-	 * 
-	 * @param id
-	 * @param title
-	 */
-	public SpeziesPage(String id, String title) {
-		super(id, title);
-	}
+    /**
+     * Create the form page.
+     * 
+     * @param id
+     * @param title
+     */
+    public SpeziesPage(String id, String title) {
+        super(id, title);
+    }
 
-	/**
-	 * Create the form page.
-	 * 
-	 * @param editor
-	 * @param id
-	 * @param title
-	 * @wbp.parser.constructor
-	 * @wbp.eval.method.parameter id "Some id"
-	 * @wbp.eval.method.parameter title "Some title"
-	 */
-	public SpeziesPage(FormEditor editor, String id, String title) {
-		super(editor, id, title);
-		this.object = Shr5Factory.eINSTANCE.createSpezies();
-	}
+    /**
+     * Create the form page.
+     * 
+     * @param editor
+     * @param id
+     * @param title
+     * @wbp.parser.constructor
+     * @wbp.eval.method.parameter id "Some id"
+     * @wbp.eval.method.parameter title "Some title"
+     */
+    public SpeziesPage(FormEditor editor, String id, String title) {
+        super(editor, id, title);
+        this.object = Shr5Factory.eINSTANCE.createSpezies();
+    }
 
-	public SpeziesPage(FormEditor editor, String id, String title,
-			Spezies object) {
-		super(editor, id, title);
-		this.object = object;
-		
-	}
+    public SpeziesPage(FormEditor editor, String id, String title, Spezies object) {
+        super(editor, id, title);
+        this.object = object;
 
-	public SpeziesPage(FormEditor editor, String id, String title, Spezies object, EditingDomain editingDomain,
-			ReferenceManager manager) {
-		super(editor, id, title, manager);
-		this.object = object;
-		this.editingDomain = editingDomain;
+    }
 
-	}
+    public SpeziesPage(FormEditor editor, String id, String title, Spezies object, EditingDomain editingDomain, ReferenceManager manager) {
+        super(editor, id, title, manager);
+        this.object = object;
+        this.editingDomain = editingDomain;
 
-	/**
-	 * Create contents of the form.
-	 * 
-	 * @param managedForm
-	 */
-	@Override
-	protected void createFormContent(IManagedForm managedForm) {
-		FormToolkit toolkit = managedForm.getToolkit();
-		ScrolledForm form = managedForm.getForm();
-		form.setText(AdapterFactoryUtil.getInstance()
-				.getLabelProvider().getText(object));
-		Composite body = form.getBody();
-		toolkit.decorateFormHeading(form.getForm());
-		toolkit.paintBordersFor(body);
-		managedForm.getForm().getBody().setLayout(new GridLayout(1, false));
+    }
 
-		BeschreibbarWidget beschreibbarWidget = new BeschreibbarWidget(
-				managedForm.getForm().getBody(), SWT.NONE, object,
-				toolkit);
-		GridData gd_beschreibbarWidget = new GridData(SWT.FILL, SWT.TOP,
-				true, false, 1, 1);
-		gd_beschreibbarWidget.widthHint = 0;
-		gd_beschreibbarWidget.heightHint = 0;
-		beschreibbarWidget.setLayoutData(gd_beschreibbarWidget);
-		managedForm.getToolkit().adapt(beschreibbarWidget);
-		managedForm.getToolkit().paintBordersFor(beschreibbarWidget);
-		
-		Group grpSpeziesBasis = new Group(managedForm.getForm().getBody(), SWT.NONE);
-		grpSpeziesBasis.setLayout(new GridLayout(6, false));
-		grpSpeziesBasis.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
-		grpSpeziesBasis.setText("Spezies Basis");
-		managedForm.getToolkit().adapt(grpSpeziesBasis);
-		managedForm.getToolkit().paintBordersFor(grpSpeziesBasis);
-		
-		Group grpLaufmodifikator = new Group(managedForm.getForm().getBody(), SWT.NONE);
-		grpLaufmodifikator.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
-		grpLaufmodifikator.setText("Laufmodifikator");
-		managedForm.getToolkit().adapt(grpLaufmodifikator);
-		managedForm.getToolkit().paintBordersFor(grpLaufmodifikator);
-		grpLaufmodifikator.setLayout(new GridLayout(9, false));
-		
-		Group grpQuelle = new Group(managedForm.getForm().getBody(), SWT.NONE);
-		grpQuelle.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
-		grpQuelle.setText("Quelle");
-		managedForm.getToolkit().adapt(grpQuelle);
-		managedForm.getToolkit().paintBordersFor(grpQuelle);
-		grpQuelle.setLayout(new GridLayout(6, false));
+    /**
+     * Create contents of the form.
+     * 
+     * @param managedForm
+     */
+    @Override
+    protected void createFormContent(IManagedForm managedForm) {
+        FormToolkit toolkit = managedForm.getToolkit();
+        ScrolledForm form = managedForm.getForm();
+        form.setText(AdapterFactoryUtil.getInstance().getLabelProvider().getText(object));
+        Composite body = form.getBody();
+        toolkit.decorateFormHeading(form.getForm());
+        toolkit.paintBordersFor(body);
+        managedForm.getForm().getBody().setLayout(new GridLayout(1, false));
 
+        BeschreibbarWidget beschreibbarWidget = new BeschreibbarWidget(managedForm.getForm().getBody(), SWT.NONE, object, toolkit);
+        GridData gd_beschreibbarWidget = new GridData(SWT.FILL, SWT.TOP, true, false, 1, 1);
+        gd_beschreibbarWidget.widthHint = 0;
+        gd_beschreibbarWidget.heightHint = 0;
+        beschreibbarWidget.setLayoutData(gd_beschreibbarWidget);
+        managedForm.getToolkit().adapt(beschreibbarWidget);
+        managedForm.getToolkit().paintBordersFor(beschreibbarWidget);
 
-		m_bindingContext = initDataBindings();
+        Group grpSpeziesBasis = new Group(managedForm.getForm().getBody(), SWT.NONE);
+        grpSpeziesBasis.setLayout(new GridLayout(6, false));
+        grpSpeziesBasis.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
+        grpSpeziesBasis.setText("Spezies Basis");
+        managedForm.getToolkit().adapt(grpSpeziesBasis);
+        managedForm.getToolkit().paintBordersFor(grpSpeziesBasis);
 
-		createFormBuilder(managedForm);
+        Group grpLaufmodifikator = new Group(managedForm.getForm().getBody(), SWT.NONE);
+        grpLaufmodifikator.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
+        grpLaufmodifikator.setText("Laufmodifikator");
+        managedForm.getToolkit().adapt(grpLaufmodifikator);
+        managedForm.getToolkit().paintBordersFor(grpLaufmodifikator);
+        grpLaufmodifikator.setLayout(new GridLayout(9, false));
 
-		emfFormBuilder.addTextEntry("Kon Min", Shr5Package.Literals.SPEZIES__KONSTITUTION_MIN, grpSpeziesBasis);
-		emfFormBuilder.addTextEntry("Kon Max", Shr5Package.Literals.SPEZIES__KONSTITUTION_MAX, grpSpeziesBasis);
+        Group grpQuelle = new Group(managedForm.getForm().getBody(), SWT.NONE);
+        grpQuelle.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
+        grpQuelle.setText("Quelle");
+        managedForm.getToolkit().adapt(grpQuelle);
+        managedForm.getToolkit().paintBordersFor(grpQuelle);
+        grpQuelle.setLayout(new GridLayout(6, false));
 
-		emfFormBuilder.addTextEntry("Ge Min", Shr5Package.Literals.SPEZIES__GESCHICKLICHKEIT_MIN, grpSpeziesBasis);
-		emfFormBuilder.addTextEntry("Ge Max", Shr5Package.Literals.SPEZIES__GESCHICKLICHKEIT_MAX, grpSpeziesBasis);
+        m_bindingContext = initDataBindings();
 
-		emfFormBuilder.addTextEntry("Rea Min", Shr5Package.Literals.SPEZIES__REAKTION_MIN, grpSpeziesBasis);
-		emfFormBuilder.addTextEntry("Rea Max", Shr5Package.Literals.SPEZIES__REAKTION_MAX, grpSpeziesBasis);
+        createFormBuilder(managedForm);
 
-		emfFormBuilder.addTextEntry("Str Min", Shr5Package.Literals.SPEZIES__STAERKE_MIN, grpSpeziesBasis);
-		emfFormBuilder.addTextEntry("Str Max", Shr5Package.Literals.SPEZIES__STAERKE_MAX, grpSpeziesBasis);
+        emfFormBuilder.addTextEntry(Shr5Package.Literals.SPEZIES__KONSTITUTION_MIN, grpSpeziesBasis);
+        emfFormBuilder.addTextEntry(Shr5Package.Literals.SPEZIES__KONSTITUTION_MAX, grpSpeziesBasis);
 
-		
-		emfFormBuilder.addTextEntry("Cha Min", Shr5Package.Literals.SPEZIES__CHARISMA_MIN, grpSpeziesBasis);
-		emfFormBuilder.addTextEntry("Cha Max", Shr5Package.Literals.SPEZIES__CHARISMA_MAX, grpSpeziesBasis);
+        emfFormBuilder.addTextEntry(Shr5Package.Literals.SPEZIES__GESCHICKLICHKEIT_MIN, grpSpeziesBasis);
+        emfFormBuilder.addTextEntry(Shr5Package.Literals.SPEZIES__GESCHICKLICHKEIT_MAX, grpSpeziesBasis);
 
-		emfFormBuilder.addTextEntry("Int Min", Shr5Package.Literals.SPEZIES__INTUITION_MIN, grpSpeziesBasis);
-		emfFormBuilder.addTextEntry("Int Max", Shr5Package.Literals.SPEZIES__INTUITION_MAX, grpSpeziesBasis);
+        emfFormBuilder.addTextEntry(Shr5Package.Literals.SPEZIES__REAKTION_MIN, grpSpeziesBasis);
+        emfFormBuilder.addTextEntry(Shr5Package.Literals.SPEZIES__REAKTION_MAX, grpSpeziesBasis);
 
-		emfFormBuilder.addTextEntry("Lo Min", Shr5Package.Literals.SPEZIES__LOGIK_MIN, grpSpeziesBasis);
-		emfFormBuilder.addTextEntry("Lo Max", Shr5Package.Literals.SPEZIES__LOGIK_MAX, grpSpeziesBasis);
+        emfFormBuilder.addTextEntry(Shr5Package.Literals.SPEZIES__STAERKE_MIN, grpSpeziesBasis);
+        emfFormBuilder.addTextEntry(Shr5Package.Literals.SPEZIES__STAERKE_MAX, grpSpeziesBasis);
 
-		emfFormBuilder.addTextEntry("Wil Min", Shr5Package.Literals.SPEZIES__WILLENSKRAFT_MIN, grpSpeziesBasis);
-		emfFormBuilder.addTextEntry("Wil Max", Shr5Package.Literals.SPEZIES__WILLENSKRAFT_MAX, grpSpeziesBasis);
+        emfFormBuilder.addTextEntry(Shr5Package.Literals.SPEZIES__CHARISMA_MIN, grpSpeziesBasis);
+        emfFormBuilder.addTextEntry(Shr5Package.Literals.SPEZIES__CHARISMA_MAX, grpSpeziesBasis);
 
-		
-		emfFormBuilder.addTextEntry("Edge Min", Shr5Package.Literals.SPEZIES__EDGE_MIN, grpSpeziesBasis);
-		emfFormBuilder.addTextEntry("Edge Max", Shr5Package.Literals.SPEZIES__EDGE_MAX, grpSpeziesBasis);
+        emfFormBuilder.addTextEntry(Shr5Package.Literals.SPEZIES__INTUITION_MIN, grpSpeziesBasis);
+        emfFormBuilder.addTextEntry(Shr5Package.Literals.SPEZIES__INTUITION_MAX, grpSpeziesBasis);
 
-		emfFormBuilder.addTextEntry("Magie Min", Shr5Package.Literals.SPEZIES__MAGIE_MIN, grpSpeziesBasis);
-		emfFormBuilder.addTextEntry("Magie Max", Shr5Package.Literals.SPEZIES__MAGIE_MAX, grpSpeziesBasis);
+        emfFormBuilder.addTextEntry(Shr5Package.Literals.SPEZIES__LOGIK_MIN, grpSpeziesBasis);
+        emfFormBuilder.addTextEntry(Shr5Package.Literals.SPEZIES__LOGIK_MAX, grpSpeziesBasis);
 
-		emfFormBuilder.addTextEntry("Resonanz Min", Shr5Package.Literals.SPEZIES__RESONANZ_MIN, grpSpeziesBasis);
-		emfFormBuilder.addTextEntry("Resonanz Max", Shr5Package.Literals.SPEZIES__RESONANZ_MAX, grpSpeziesBasis);
+        emfFormBuilder.addTextEntry(Shr5Package.Literals.SPEZIES__WILLENSKRAFT_MIN, grpSpeziesBasis);
+        emfFormBuilder.addTextEntry(Shr5Package.Literals.SPEZIES__WILLENSKRAFT_MAX, grpSpeziesBasis);
 
-		emfFormBuilder.addTextEntry("Essenz Min", Shr5Package.Literals.SPEZIES__ESSENZ_MIN, grpSpeziesBasis);
-		emfFormBuilder.addTextEntry("Essenz Max", Shr5Package.Literals.SPEZIES__ESSENZ_MAX, grpSpeziesBasis);
+        emfFormBuilder.addTextEntry(Shr5Package.Literals.SPEZIES__EDGE_MIN, grpSpeziesBasis);
+        emfFormBuilder.addTextEntry(Shr5Package.Literals.SPEZIES__EDGE_MAX, grpSpeziesBasis);
 
-		
-		
-		emfFormBuilder.addTextEntry("Laufen", Shr5Package.Literals.SPEZIES__LAUFEN, grpLaufmodifikator);
-		emfFormBuilder.addTextEntry("Rennen", Shr5Package.Literals.SPEZIES__RENNEN, grpLaufmodifikator);
-		emfFormBuilder.addTextEntry("Sprinten", Shr5Package.Literals.SPEZIES__SPRINTEN, grpLaufmodifikator);
+        emfFormBuilder.addTextEntry(Shr5Package.Literals.SPEZIES__MAGIE_MIN, grpSpeziesBasis);
+        emfFormBuilder.addTextEntry(Shr5Package.Literals.SPEZIES__MAGIE_MAX, grpSpeziesBasis);
 
-		emfFormBuilder.addTextEntry("Source", Shr5Package.Literals.QUELLE__SRC_BOOK, grpQuelle);
-		emfFormBuilder.addTextEntry("Page", Shr5Package.Literals.QUELLE__PAGE, grpQuelle);
+        emfFormBuilder.addTextEntry(Shr5Package.Literals.SPEZIES__RESONANZ_MIN, grpSpeziesBasis);
+        emfFormBuilder.addTextEntry(Shr5Package.Literals.SPEZIES__RESONANZ_MAX, grpSpeziesBasis);
 
-		emfFormBuilder.buildinComposite(m_bindingContext, managedForm.getForm().getBody(), object);
-		managedForm.reflow(true);
-	}
-	
+        emfFormBuilder.addTextEntry(Shr5Package.Literals.SPEZIES__ESSENZ_MIN, grpSpeziesBasis);
+        emfFormBuilder.addTextEntry(Shr5Package.Literals.SPEZIES__ESSENZ_MAX, grpSpeziesBasis);
 
-	protected DataBindingContext initDataBindings() {
-		DataBindingContext bindingContext = new DataBindingContext();
-		//
-		return bindingContext;
-	}
+        emfFormBuilder.addTextEntry(Shr5Package.Literals.SPEZIES__LAUFEN, grpLaufmodifikator);
+        emfFormBuilder.addTextEntry(Shr5Package.Literals.SPEZIES__RENNEN, grpLaufmodifikator);
+        emfFormBuilder.addTextEntry(Shr5Package.Literals.SPEZIES__SPRINTEN, grpLaufmodifikator);
 
-	@Override
-	protected EditingDomain getEditingDomain() {
-		return editingDomain;
-	}
+        emfFormBuilder.addTextEntry(Shr5Package.Literals.QUELLE__SRC_BOOK, grpQuelle);
+        emfFormBuilder.addTextEntry(Shr5Package.Literals.QUELLE__PAGE, grpQuelle);
+
+        emfFormBuilder.buildinComposite(m_bindingContext, managedForm.getForm().getBody(), object);
+        managedForm.reflow(true);
+    }
+
+    protected DataBindingContext initDataBindings() {
+        DataBindingContext bindingContext = new DataBindingContext();
+        //
+        return bindingContext;
+    }
+
+    @Override
+    protected EditingDomain getEditingDomain() {
+        return editingDomain;
+    }
 }
