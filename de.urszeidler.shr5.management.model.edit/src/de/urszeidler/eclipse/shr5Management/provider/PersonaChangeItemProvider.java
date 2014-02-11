7ea09c2aa11a50c212ed28219bcecdb5063a202e
UrsZeidler <github@urszeidler.de>
2014-02-11 18:05:50 +0100

    update the item provider

--- de.urszeidler.shr5.management.model.edit/src/de/urszeidler/eclipse/shr5Management/provider/PersonaChangeItemProvider.java
+++ de.urszeidler.shr5.management.model.edit/src/de/urszeidler/eclipse/shr5Management/provider/PersonaChangeItemProvider.java
@@ -3,9 +3,7 @@
  */
 package de.urszeidler.eclipse.shr5Management.provider;
 
-
 import java.util.Collection;
-import java.util.Date;
 import java.util.List;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
@@ -25,20 +23,16 @@ import de.urszeidler.eclipse.shr5Management.Shr5managementPackage;
  * This is the item provider adapter for a {@link de.urszeidler.eclipse.shr5Management.PersonaChange} object.
  * <!-- begin-user-doc -->
  * <!-- end-user-doc -->
+ * 
  * @generated
  */
-public class PersonaChangeItemProvider
-    extends PersonaValueChangeItemProvider
-    implements
-        IEditingDomainItemProvider,
-        IStructuredItemContentProvider,
-        ITreeItemContentProvider,
-        IItemLabelProvider,
-        IItemPropertySource {
+public class PersonaChangeItemProvider extends PersonaValueChangeItemProvider implements IEditingDomainItemProvider, IStructuredItemContentProvider,
+        ITreeItemContentProvider, IItemLabelProvider, IItemPropertySource {
     /**
      * This constructs an instance from a factory and a notifier.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
+     * 
      * @generated
      */
     public PersonaChangeItemProvider(AdapterFactory adapterFactory) {
@@ -49,6 +43,7 @@ public class PersonaChangeItemProvider
      * This returns the property descriptors for the adapted class.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
+     * 
      * @generated
      */
     @Override
@@ -65,28 +60,21 @@ public class PersonaChangeItemProvider
      * This adds a property descriptor for the Changeable feature.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
+     * 
      * @generated
      */
     protected void addChangeablePropertyDescriptor(Object object) {
-        itemPropertyDescriptors.add
-            (createItemPropertyDescriptor
-                (((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
-                 getResourceLocator(),
-                 getString("_UI_PersonaChange_changeable_feature"),
-                 getString("_UI_PropertyDescriptor_description", "_UI_PersonaChange_changeable_feature", "_UI_PersonaChange_type"),
-                 Shr5managementPackage.Literals.PERSONA_CHANGE__CHANGEABLE,
-                 true,
-                 false,
-                 true,
-                 null,
-                 null,
-                 null));
+        itemPropertyDescriptors.add(createItemPropertyDescriptor(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+                getResourceLocator(), getString("_UI_PersonaChange_changeable_feature"),
+                getString("_UI_PropertyDescriptor_description", "_UI_PersonaChange_changeable_feature", "_UI_PersonaChange_type"),
+                Shr5managementPackage.Literals.PERSONA_CHANGE__CHANGEABLE, true, false, true, null, null, null));
     }
 
     /**
      * This returns PersonaChange.gif.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
+     * 
      * @generated
      */
     @Override
@@ -98,15 +86,22 @@ public class PersonaChangeItemProvider
      * This returns the label text for the adapted class.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
+     * 
+     * @generated not
      */
     @Override
     public String getText(Object object) {
-        Date labelValue = ((PersonaChange)object).getDate();
+        PersonaChange personaChange = (PersonaChange)object;
+        ComposeableAdapterFactory factory = ((Shr5managementItemProviderAdapterFactory)this.adapterFactory).getRootAdapterFactory();
+        String labelValue = "";
+        if (factory != null && personaChange.getChangeable() != null) {
+            IItemLabelProvider labelprovider = (IItemLabelProvider)factory.adapt(personaChange.getChangeable(), IItemLabelProvider.class);
+            if (labelprovider != null)
+                labelValue = labelprovider.getText(personaChange.getChangeable()) + personaChange.getFrom() + " -> " + personaChange.getTo();
+        }
+
         String label = labelValue == null ? null : labelValue.toString();
-        return label == null || label.length() == 0 ?
-            getString("_UI_PersonaChange_type") :
-            getString("_UI_PersonaChange_type") + " " + label;
+        return label == null || label.length() == 0 ? getString("_UI_PersonaChange_type") : getString("_UI_PersonaChange_type") + " " + label;
     }
 
     /**
@@ -114,6 +109,7 @@ public class PersonaChangeItemProvider
      * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
+     * 
      * @generated
      */
     @Override
@@ -127,6 +123,7 @@ public class PersonaChangeItemProvider
      * that can be created under this object.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
+     * 
      * @generated
      */
     @Override
