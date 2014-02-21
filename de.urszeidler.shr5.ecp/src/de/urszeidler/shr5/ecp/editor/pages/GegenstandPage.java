ad170580a1df9f02cdeb100b60d82aeb01b2abab
UrsZeidler <github@urszeidler.de>
2014-02-21 17:20:47 +0100

    delegate the label to the feature name and added the credstick

--- de.urszeidler.shr5.ecp/src/de/urszeidler/shr5/ecp/editor/pages/GegenstandPage.java
+++ de.urszeidler.shr5.ecp/src/de/urszeidler/shr5/ecp/editor/pages/GegenstandPage.java
@@ -14,6 +14,7 @@ import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.ui.forms.widgets.ScrolledForm;
 
 import de.urszeidler.eclipse.shr5.AbstraktGegenstand;
+import de.urszeidler.eclipse.shr5.Credstick;
 import de.urszeidler.eclipse.shr5.Gegenstand;
 import de.urszeidler.eclipse.shr5.Kleidung;
 import de.urszeidler.eclipse.shr5.Shr5Factory;
@@ -24,126 +25,129 @@ import de.urszeidler.shr5.ecp.editor.widgets.BeschreibbarWidget;
 
 /**
  * Manages {@link Gegenstand} and {@link Kleidung}.
+ * 
  * @author urs
- *
  */
 public class GegenstandPage extends AbstractShr5Page<AbstraktGegenstand> {
-	private AbstraktGegenstand object;
-	private EditingDomain editingDomain;
+    private AbstraktGegenstand object;
+    private EditingDomain editingDomain;
 
-	private DataBindingContext m_bindingContext;
+    private DataBindingContext m_bindingContext;
 
-	/**
-	 * Create the form page.
-	 * 
-	 * @param id
-	 * @param title
-	 */
-	public GegenstandPage(String id, String title) {
-		super(id, title);
-	}
+    /**
+     * Create the form page.
+     * 
+     * @param id
+     * @param title
+     */
+    public GegenstandPage(String id, String title) {
+        super(id, title);
+    }
 
-	/**
-	 * Create the form page.
-	 * 
-	 * @param editor
-	 * @param id
-	 * @param title
-	 * @wbp.parser.constructor
-	 * @wbp.eval.method.parameter id "Some id"
-	 * @wbp.eval.method.parameter title "Some title"
-	 */
-	public GegenstandPage(FormEditor editor, String id, String title) {
-		super(editor, id, title);
-		this.object = Shr5Factory.eINSTANCE.createGegenstand();
-	}
+    /**
+     * Create the form page.
+     * 
+     * @param editor
+     * @param id
+     * @param title
+     * @wbp.parser.constructor
+     * @wbp.eval.method.parameter id "Some id"
+     * @wbp.eval.method.parameter title "Some title"
+     */
+    public GegenstandPage(FormEditor editor, String id, String title) {
+        super(editor, id, title);
+        this.object = Shr5Factory.eINSTANCE.createGegenstand();
+    }
 
-	public GegenstandPage(FormEditor editor, String id, String title, AbstraktGegenstand object, EditingDomain editingDomain,
-			ReferenceManager manager) {
-		super(editor, id, title, manager);
-		this.object = object;
-		this.editingDomain = editingDomain;
+    public GegenstandPage(FormEditor editor, String id, String title, AbstraktGegenstand object, EditingDomain editingDomain, ReferenceManager manager) {
+        super(editor, id, title, manager);
+        this.object = object;
+        this.editingDomain = editingDomain;
 
-	}
+    }
 
-	/**
-	 * Create contents of the form.
-	 * 
-	 * @param managedForm
-	 */
-	@Override
-	protected void createFormContent(IManagedForm managedForm) {
-		FormToolkit toolkit = managedForm.getToolkit();
-		ScrolledForm form = managedForm.getForm();
-		form.setText(AdapterFactoryUtil.getInstance().getLabelProvider().getText(object));
-		Composite body = form.getBody();
-		toolkit.decorateFormHeading(form.getForm());
-		toolkit.paintBordersFor(body);
-		managedForm.getForm().getBody().setLayout(new GridLayout(1, false));
+    /**
+     * Create contents of the form.
+     * 
+     * @param managedForm
+     */
+    @Override
+    protected void createFormContent(IManagedForm managedForm) {
+        FormToolkit toolkit = managedForm.getToolkit();
+        ScrolledForm form = managedForm.getForm();
+        form.setText(AdapterFactoryUtil.getInstance().getLabelProvider().getText(object));
+        Composite body = form.getBody();
+        toolkit.decorateFormHeading(form.getForm());
+        toolkit.paintBordersFor(body);
+        managedForm.getForm().getBody().setLayout(new GridLayout(1, false));
 
-		BeschreibbarWidget beschreibbarWidget = new BeschreibbarWidget(managedForm.getForm().getBody(), SWT.NONE, object, toolkit);
-		GridData gd_beschreibbarWidget = new GridData(SWT.FILL, SWT.TOP, true, false, 1, 1);
-		gd_beschreibbarWidget.widthHint = 0;
-		gd_beschreibbarWidget.heightHint = 0;
-		beschreibbarWidget.setLayoutData(gd_beschreibbarWidget);
-		managedForm.getToolkit().adapt(beschreibbarWidget);
-		managedForm.getToolkit().paintBordersFor(beschreibbarWidget);
+        BeschreibbarWidget beschreibbarWidget = new BeschreibbarWidget(managedForm.getForm().getBody(), SWT.NONE, object, toolkit);
+        GridData gd_beschreibbarWidget = new GridData(SWT.FILL, SWT.TOP, true, false, 1, 1);
+        gd_beschreibbarWidget.widthHint = 0;
+        gd_beschreibbarWidget.heightHint = 0;
+        beschreibbarWidget.setLayoutData(gd_beschreibbarWidget);
+        managedForm.getToolkit().adapt(beschreibbarWidget);
+        managedForm.getToolkit().paintBordersFor(beschreibbarWidget);
 
-		Group grpGegenstand = new Group(managedForm.getForm().getBody(), SWT.NONE);
-		grpGegenstand.setLayout(new GridLayout(6, false));
-		grpGegenstand.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-		managedForm.getToolkit().adapt(grpGegenstand);
-		managedForm.getToolkit().paintBordersFor(grpGegenstand);
+        Group grpGegenstand = new Group(managedForm.getForm().getBody(), SWT.NONE);
+        grpGegenstand.setLayout(new GridLayout(6, false));
+        grpGegenstand.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+        managedForm.getToolkit().adapt(grpGegenstand);
+        managedForm.getToolkit().paintBordersFor(grpGegenstand);
 
-		Composite composite = new Composite(managedForm.getForm().getBody(), SWT.NONE);
-		composite.setLayout(new FillLayout(SWT.HORIZONTAL));
-		composite.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-		managedForm.getToolkit().adapt(composite);
-		managedForm.getToolkit().paintBordersFor(composite);
+        Composite composite = new Composite(managedForm.getForm().getBody(), SWT.NONE);
+        composite.setLayout(new FillLayout(SWT.HORIZONTAL));
+        composite.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+        managedForm.getToolkit().adapt(composite);
+        managedForm.getToolkit().paintBordersFor(composite);
 
-		Group grpWert = new Group(composite, SWT.NONE);
-		grpWert.setText("Wert");
-		managedForm.getToolkit().adapt(grpWert);
-		managedForm.getToolkit().paintBordersFor(grpWert);
-		grpWert.setLayout(new GridLayout(6, false));
+        Group grpWert = new Group(composite, SWT.NONE);
+        grpWert.setText("Wert");
+        managedForm.getToolkit().adapt(grpWert);
+        managedForm.getToolkit().paintBordersFor(grpWert);
+        grpWert.setLayout(new GridLayout(6, false));
 
-		Group grpQuelle = new Group(composite, SWT.NONE);
-		grpQuelle.setText("Quelle");
-		managedForm.getToolkit().adapt(grpQuelle);
-		managedForm.getToolkit().paintBordersFor(grpQuelle);
-		grpQuelle.setLayout(new GridLayout(6, false));
+        Group grpQuelle = new Group(composite, SWT.NONE);
+        grpQuelle.setText("Quelle");
+        managedForm.getToolkit().adapt(grpQuelle);
+        managedForm.getToolkit().paintBordersFor(grpQuelle);
+        grpQuelle.setLayout(new GridLayout(6, false));
 
-		m_bindingContext = initDataBindings();
+        m_bindingContext = initDataBindings();
 
-		createFormBuilder(managedForm);
+        createFormBuilder(managedForm);
 
-		if (object instanceof Gegenstand) {
-			grpGegenstand.setText("Gegenstand");
-			emfFormBuilder.addTextEntry("Kategorie", Shr5Package.Literals.GEGENSTAND__KATEGORIE, grpGegenstand);
-			emfFormBuilder.addTextEntry("Stufe", Shr5Package.Literals.GEGENSTAND__STUFE, grpGegenstand);
-		} else if (object instanceof Kleidung) {
-			grpGegenstand.setText("Rüstung");
-			emfFormBuilder.addTextEntry("Panzer Wert", Shr5Package.Literals.KLEIDUNG__RUESTUNG, grpGegenstand);
+        if (object instanceof Gegenstand) {
+            grpGegenstand.setText("Gegenstand");
+            emfFormBuilder.addTextEntry(Shr5Package.Literals.GEGENSTAND__KATEGORIE, grpGegenstand);
+            emfFormBuilder.addTextEntry(Shr5Package.Literals.GEGENSTAND__STUFE, grpGegenstand);
+        } else if (object instanceof Kleidung) {
+            grpGegenstand.setText("Rüstung");
+            emfFormBuilder.addTextEntry(Shr5Package.Literals.KLEIDUNG__RUESTUNG, grpGegenstand);
+        } else if (object instanceof Credstick) {
+            grpGegenstand.setText("Credstick");
+            emfFormBuilder.addTextEntry(Shr5Package.Literals.CREDSTICK__CURREN_VALUE, grpGegenstand);
+            emfFormBuilder.addTextEntry(Shr5Package.Literals.CREDSTICK__MAX_VALUE, grpGegenstand);
+        }
 
-		}
-		emfFormBuilder.addTextEntry("Wert", Shr5Package.Literals.GELD_WERT__WERT, grpWert);
-		emfFormBuilder.addTextEntry("Verfügbarkeit", Shr5Package.Literals.GELD_WERT__VERFUEGBARKEIT, grpWert);
+        emfFormBuilder.addTextEntry(Shr5Package.Literals.GELD_WERT__WERT, grpWert);
+        emfFormBuilder.addTextEntry(Shr5Package.Literals.GELD_WERT__VERFUEGBARKEIT, grpWert);
 
-		emfFormBuilder.addTextEntry("Source", Shr5Package.Literals.QUELLE__SRC_BOOK, grpQuelle);
-		emfFormBuilder.addTextEntry("Page", Shr5Package.Literals.QUELLE__PAGE, grpQuelle);
+        emfFormBuilder.addTextEntry(Shr5Package.Literals.QUELLE__SRC_BOOK, grpQuelle);
+        emfFormBuilder.addTextEntry(Shr5Package.Literals.QUELLE__PAGE, grpQuelle);
 
-		emfFormBuilder.buildinComposite(m_bindingContext, managedForm.getForm().getBody(), object);
-		managedForm.reflow(true);
-	}
+        emfFormBuilder.buildinComposite(m_bindingContext, managedForm.getForm().getBody(), object);
+        managedForm.reflow(true);
+    }
 
-	protected DataBindingContext initDataBindings() {
-		DataBindingContext bindingContext = new DataBindingContext();
-		//
-		return bindingContext;
-	}
+    protected DataBindingContext initDataBindings() {
+        DataBindingContext bindingContext = new DataBindingContext();
+        //
+        return bindingContext;
+    }
 
-	@Override
-	protected EditingDomain getEditingDomain() {
-		return editingDomain;
-	}
+    @Override
+    protected EditingDomain getEditingDomain() {
+        return editingDomain;
+    }
 }
