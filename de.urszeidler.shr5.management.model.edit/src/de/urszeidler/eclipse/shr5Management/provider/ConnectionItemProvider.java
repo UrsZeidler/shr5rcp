74cc81b3e6205bb1d640d818d4e87d6fcb7b4903
UrsZeidler <github@urszeidler.de>
2014-03-08 15:38:52 +0100

    customized the item provider of the connection

--- de.urszeidler.shr5.management.model.edit/src/de/urszeidler/eclipse/shr5Management/provider/ConnectionItemProvider.java
+++ de.urszeidler.shr5.management.model.edit/src/de/urszeidler/eclipse/shr5Management/provider/ConnectionItemProvider.java
@@ -3,7 +3,6 @@
  */
 package de.urszeidler.eclipse.shr5Management.provider;
 
-
 import java.util.Collection;
 import java.util.List;
 
@@ -28,34 +27,31 @@ import de.urszeidler.eclipse.shr5Management.Shr5managementPackage;
  * This is the item provider adapter for a {@link de.urszeidler.eclipse.shr5Management.Connection} object.
  * <!-- begin-user-doc -->
  * <!-- end-user-doc -->
+ * 
  * @generated
  */
-public class ConnectionItemProvider
-	extends ItemProviderAdapter
-	implements
-		IEditingDomainItemProvider,
-		IStructuredItemContentProvider,
-		ITreeItemContentProvider,
-		IItemLabelProvider,
-		IItemPropertySource {
-	/**
+public class ConnectionItemProvider extends ItemProviderAdapter implements IEditingDomainItemProvider, IStructuredItemContentProvider,
+        ITreeItemContentProvider, IItemLabelProvider, IItemPropertySource {
+    /**
      * This constructs an instance from a factory and a notifier.
      * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+     * <!-- end-user-doc -->
+     * 
      * @generated
      */
-	public ConnectionItemProvider(AdapterFactory adapterFactory) {
+    public ConnectionItemProvider(AdapterFactory adapterFactory) {
         super(adapterFactory);
     }
 
-	/**
+    /**
      * This returns the property descriptors for the adapted class.
      * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+     * <!-- end-user-doc -->
+     * 
      * @generated
      */
-	@Override
-	public List<IItemPropertyDescriptor> getPropertyDescriptors(Object object) {
+    @Override
+    public List<IItemPropertyDescriptor> getPropertyDescriptors(Object object) {
         if (itemPropertyDescriptors == null) {
             super.getPropertyDescriptors(object);
 
@@ -66,104 +62,104 @@ public class ConnectionItemProvider
         return itemPropertyDescriptors;
     }
 
-	/**
+    /**
      * This adds a property descriptor for the Influence feature.
      * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+     * <!-- end-user-doc -->
+     * 
      * @generated
      */
-	protected void addInfluencePropertyDescriptor(Object object) {
-        itemPropertyDescriptors.add
-            (createItemPropertyDescriptor
-                (((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
-                 getResourceLocator(),
-                 getString("_UI_Connection_influence_feature"),
-                 getString("_UI_PropertyDescriptor_description", "_UI_Connection_influence_feature", "_UI_Connection_type"),
-                 Shr5managementPackage.Literals.CONNECTION__INFLUENCE,
-                 true,
-                 false,
-                 false,
-                 ItemPropertyDescriptor.INTEGRAL_VALUE_IMAGE,
-                 null,
-                 null));
+    protected void addInfluencePropertyDescriptor(Object object) {
+        itemPropertyDescriptors.add(createItemPropertyDescriptor(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+                getResourceLocator(), getString("_UI_Connection_influence_feature"),
+                getString("_UI_PropertyDescriptor_description", "_UI_Connection_influence_feature", "_UI_Connection_type"),
+                Shr5managementPackage.Literals.CONNECTION__INFLUENCE, true, false, false, ItemPropertyDescriptor.INTEGRAL_VALUE_IMAGE, null, null));
     }
 
-	/**
+    /**
      * This adds a property descriptor for the Loyality feature.
      * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+     * <!-- end-user-doc -->
+     * 
      * @generated
      */
-	protected void addLoyalityPropertyDescriptor(Object object) {
-        itemPropertyDescriptors.add
-            (createItemPropertyDescriptor
-                (((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
-                 getResourceLocator(),
-                 getString("_UI_Connection_loyality_feature"),
-                 getString("_UI_PropertyDescriptor_description", "_UI_Connection_loyality_feature", "_UI_Connection_type"),
-                 Shr5managementPackage.Literals.CONNECTION__LOYALITY,
-                 true,
-                 false,
-                 false,
-                 ItemPropertyDescriptor.INTEGRAL_VALUE_IMAGE,
-                 null,
-                 null));
+    protected void addLoyalityPropertyDescriptor(Object object) {
+        itemPropertyDescriptors.add(createItemPropertyDescriptor(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+                getResourceLocator(), getString("_UI_Connection_loyality_feature"),
+                getString("_UI_PropertyDescriptor_description", "_UI_Connection_loyality_feature", "_UI_Connection_type"),
+                Shr5managementPackage.Literals.CONNECTION__LOYALITY, true, false, false, ItemPropertyDescriptor.INTEGRAL_VALUE_IMAGE, null, null));
     }
 
-	/**
+    /**
      * This adds a property descriptor for the Character feature.
      * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+     * <!-- end-user-doc -->
+     * 
      * @generated
      */
-	protected void addCharacterPropertyDescriptor(Object object) {
-        itemPropertyDescriptors.add
-            (createItemPropertyDescriptor
-                (((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
-                 getResourceLocator(),
-                 getString("_UI_Connection_character_feature"),
-                 getString("_UI_PropertyDescriptor_description", "_UI_Connection_character_feature", "_UI_Connection_type"),
-                 Shr5managementPackage.Literals.CONNECTION__CHARACTER,
-                 true,
-                 false,
-                 true,
-                 null,
-                 null,
-                 null));
+    protected void addCharacterPropertyDescriptor(Object object) {
+        itemPropertyDescriptors.add(createItemPropertyDescriptor(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+                getResourceLocator(), getString("_UI_Connection_character_feature"),
+                getString("_UI_PropertyDescriptor_description", "_UI_Connection_character_feature", "_UI_Connection_type"),
+                Shr5managementPackage.Literals.CONNECTION__CHARACTER, true, false, true, null, null, null));
     }
 
-	/**
+    /**
      * This returns Connection.gif.
      * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-     * @generated
+     * <!-- end-user-doc -->
+     * 
+     * @generated not
      */
-	@Override
-	public Object getImage(Object object) {
+    @Override
+    public Object getImage(Object object) {
+        Connection connection = (Connection)object;
+        ComposeableAdapterFactory factory = ((Shr5managementItemProviderAdapterFactory) this.adapterFactory).getRootAdapterFactory();
+        if (factory != null && connection.getCharacter() != null) {
+            IItemLabelProvider labelprovider = (IItemLabelProvider) factory.adapt(connection.getCharacter(),
+                    IItemLabelProvider.class);
+            if (labelprovider != null)
+                return labelprovider.getImage(connection.getCharacter());
+        }
+
+        
+        
         return overlayImage(object, getResourceLocator().getImage("full/obj16/Connection"));
     }
 
-	/**
+    /**
      * This returns the label text for the adapted class.
      * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-     * @generated
+     * <!-- end-user-doc -->
+     * 
+     * @generated not
      */
-	@Override
-	public String getText(Object object) {
+    @Override
+    public String getText(Object object) {
         Connection connection = (Connection)object;
-        return getString("_UI_Connection_type") + " " + connection.getInfluence();
+        String label = " unset";
+        if (connection.getCharacter() != null) {
+            ComposeableAdapterFactory factory = ((Shr5managementItemProviderAdapterFactory)this.adapterFactory).getRootAdapterFactory();
+            IItemLabelProvider labelprovider = (IItemLabelProvider)factory.adapt(connection.getCharacter(), IItemLabelProvider.class);
+
+            if (labelprovider != null) {
+                label = labelprovider.getText(connection);
+            }
+        }
+
+        return getString("_UI_Connection_type") + " " + label + " [" + connection.getInfluence() + "/" + connection.getLoyality() + "]";
     }
 
-	/**
+    /**
      * This handles model notifications by calling {@link #updateChildren} to update any cached
      * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
      * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+     * <!-- end-user-doc -->
+     * 
      * @generated
      */
-	@Override
-	public void notifyChanged(Notification notification) {
+    @Override
+    public void notifyChanged(Notification notification) {
         updateChildren(notification);
 
         switch (notification.getFeatureID(Connection.class)) {
@@ -175,26 +171,28 @@ public class ConnectionItemProvider
         super.notifyChanged(notification);
     }
 
-	/**
+    /**
      * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children
      * that can be created under this object.
      * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+     * <!-- end-user-doc -->
+     * 
      * @generated
      */
-	@Override
-	protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object) {
+    @Override
+    protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object) {
         super.collectNewChildDescriptors(newChildDescriptors, object);
     }
 
-	/**
+    /**
      * Return the resource locator for this item provider's resources.
      * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+     * <!-- end-user-doc -->
+     * 
      * @generated
      */
-	@Override
-	public ResourceLocator getResourceLocator() {
+    @Override
+    public ResourceLocator getResourceLocator() {
         return Shr5ManagementEditPlugin.INSTANCE;
     }
 
