76200161b92e946241947a085e995ec8137934e0
UrsZeidler <github@urszeidler.de>
2014-05-14 11:31:48 +0200

    added pan as an object of a matrix device

--- de.urszeidler.shr5.model.edit/src/de/urszeidler/eclipse/shr5/provider/PersonalAreaNetworkItemProvider.java
+++ de.urszeidler.shr5.model.edit/src/de/urszeidler/eclipse/shr5/provider/PersonalAreaNetworkItemProvider.java
@@ -5,19 +5,12 @@ package de.urszeidler.eclipse.shr5.provider;
 
 
 import de.urszeidler.eclipse.shr5.PersonalAreaNetwork;
-import de.urszeidler.eclipse.shr5.Shr5Factory;
 import de.urszeidler.eclipse.shr5.Shr5Package;
-
 import java.util.Collection;
 import java.util.List;
-
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
-
 import org.eclipse.emf.common.util.ResourceLocator;
-
-import org.eclipse.emf.ecore.EStructuralFeature;
-
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
 import org.eclipse.emf.edit.provider.IItemLabelProvider;
@@ -64,9 +57,32 @@ public class PersonalAreaNetworkItemProvider
         if (itemPropertyDescriptors == null) {
             super.getPropertyDescriptors(object);
 
+            addSlavesPropertyDescriptor(object);
             addSlaveMaxPropertyDescriptor(object);
         }
         return itemPropertyDescriptors;
+    }
+
+    /**
+     * This adds a property descriptor for the Slaves feature.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    protected void addSlavesPropertyDescriptor(Object object) {
+        itemPropertyDescriptors.add
+            (createItemPropertyDescriptor
+                (((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+                 getResourceLocator(),
+                 getString("_UI_PersonalAreaNetwork_slaves_feature"),
+                 getString("_UI_PropertyDescriptor_description", "_UI_PersonalAreaNetwork_slaves_feature", "_UI_PersonalAreaNetwork_type"),
+                 Shr5Package.Literals.PERSONAL_AREA_NETWORK__SLAVES,
+                 true,
+                 false,
+                 true,
+                 null,
+                 null,
+                 null));
     }
 
     /**
@@ -89,36 +105,6 @@ public class PersonalAreaNetworkItemProvider
                  ItemPropertyDescriptor.INTEGRAL_VALUE_IMAGE,
                  null,
                  null));
-    }
-
-    /**
-     * This specifies how to implement {@link #getChildren} and is used to deduce an appropriate feature for an
-     * {@link org.eclipse.emf.edit.command.AddCommand}, {@link org.eclipse.emf.edit.command.RemoveCommand} or
-     * {@link org.eclipse.emf.edit.command.MoveCommand} in {@link #createCommand}.
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @generated
-     */
-    @Override
-    public Collection<? extends EStructuralFeature> getChildrenFeatures(Object object) {
-        if (childrenFeatures == null) {
-            super.getChildrenFeatures(object);
-            childrenFeatures.add(Shr5Package.Literals.PERSONAL_AREA_NETWORK__SLAVES);
-        }
-        return childrenFeatures;
-    }
-
-    /**
-     * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-     * @generated
-     */
-    @Override
-    protected EStructuralFeature getChildFeature(Object object, Object child) {
-        // Check the type of the specified child object and return the proper feature to use for
-        // adding (see {@link AddCommand}) it as a child.
-
-        return super.getChildFeature(object, child);
     }
 
     /**
@@ -159,9 +145,6 @@ public class PersonalAreaNetworkItemProvider
             case Shr5Package.PERSONAL_AREA_NETWORK__SLAVE_MAX:
                 fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
                 return;
-            case Shr5Package.PERSONAL_AREA_NETWORK__SLAVES:
-                fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), true, false));
-                return;
         }
         super.notifyChanged(notification);
     }
@@ -176,41 +159,6 @@ public class PersonalAreaNetworkItemProvider
     @Override
     protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object) {
         super.collectNewChildDescriptors(newChildDescriptors, object);
-
-        newChildDescriptors.add
-            (createChildParameter
-                (Shr5Package.Literals.PERSONAL_AREA_NETWORK__SLAVES,
-                 Shr5Factory.eINSTANCE.createDrohne()));
-
-        newChildDescriptors.add
-            (createChildParameter
-                (Shr5Package.Literals.PERSONAL_AREA_NETWORK__SLAVES,
-                 Shr5Factory.eINSTANCE.createTechnomancer()));
-
-        newChildDescriptors.add
-            (createChildParameter
-                (Shr5Package.Literals.PERSONAL_AREA_NETWORK__SLAVES,
-                 Shr5Factory.eINSTANCE.createSprite()));
-
-        newChildDescriptors.add
-            (createChildParameter
-                (Shr5Package.Literals.PERSONAL_AREA_NETWORK__SLAVES,
-                 Shr5Factory.eINSTANCE.createCommlink()));
-
-        newChildDescriptors.add
-            (createChildParameter
-                (Shr5Package.Literals.PERSONAL_AREA_NETWORK__SLAVES,
-                 Shr5Factory.eINSTANCE.createCyberdeck()));
-
-        newChildDescriptors.add
-            (createChildParameter
-                (Shr5Package.Literals.PERSONAL_AREA_NETWORK__SLAVES,
-                 Shr5Factory.eINSTANCE.createHost()));
-
-        newChildDescriptors.add
-            (createChildParameter
-                (Shr5Package.Literals.PERSONAL_AREA_NETWORK__SLAVES,
-                 Shr5Factory.eINSTANCE.createRiggerCommandConsole()));
     }
 
     /**
