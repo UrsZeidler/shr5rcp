7564667da9052e97d0af44801b52b1c2807af76f
UrsZeidler <github@urszeidler.de>
2014-05-19 22:26:15 +0200

    added a new svg page

--- de.urszeidler.shr5.acceleo/src/de/urszeidler/shr5/acceleo/util/ShadowrunTextTools.java
+++ de.urszeidler.shr5.acceleo/src/de/urszeidler/shr5/acceleo/util/ShadowrunTextTools.java
@@ -3,6 +3,8 @@
  */
 package de.urszeidler.shr5.acceleo.util;
 
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,17 +13,21 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
 
+import de.urszeidler.eclipse.shr5.AbstraktPersona;
 import de.urszeidler.eclipse.shr5.Shr5Factory;
 import de.urszeidler.eclipse.shr5.Shr5Package;
 import de.urszeidler.eclipse.shr5.Zauber;
 import de.urszeidler.eclipse.shr5.ZauberArt;
 import de.urszeidler.eclipse.shr5.util.AdapterFactoryUtil;
+import de.urszeidler.eclipse.shr5Management.provider.Shr5ManagementEditPlugin;
+import de.urszeidler.shr5.acceleo.Activator;
 
 /**
  * @author urs
  */
 public class ShadowrunTextTools {
 
+    private static final String EMPTY = "";
     private static Zauber zauber = Shr5Factory.eINSTANCE.createZauber();
 
     /**
@@ -50,7 +56,7 @@ public class ShadowrunTextTools {
      */
     public static String toName(Object literal, EObject eobject, EAttribute feature) {
         if (literal == null)
-            return "";
+            return EMPTY;
 
         String text2 = literal.toString();
         IItemPropertyDescriptor propertyDescriptor = AdapterFactoryUtil.getInstance().getItemDelegator().getPropertyDescriptor(eobject, feature);
@@ -69,7 +75,7 @@ public class ShadowrunTextTools {
      */
     public static String zauberArtToName(ZauberArt literal) {
         if (literal == null)
-            return "";
+            return EMPTY;
 
         String text2 = literal.toString();
         IItemPropertyDescriptor propertyDescriptor = AdapterFactoryUtil.getInstance().getItemDelegator()
@@ -90,8 +96,75 @@ public class ShadowrunTextTools {
     public static List<String> createNumberList(Integer count) {
         ArrayList<String> list = new ArrayList<String>(count);
         for (int i = 1; i < count + 1; i++) {
-            list.add(i + "");
+            list.add(i + EMPTY);
         }
         return list;
+    }
+
+    /**
+     * Get a localized text from the managenent plugin with the prefix _PR_.
+     * 
+     * @param string
+     * @return
+     */
+    public static String localizedString(String string) {
+        try {
+            String loc = Shr5ManagementEditPlugin.INSTANCE.getString("_PR_" + string, true);
+            return loc;
+        } catch (Exception e) {
+            return string;
+        }
+    }
+
+    /**
+     * Looksup the initiative as localized string.
+     * 
+     * @param persona
+     * @return
+     */
+    public static String toLocalizedInitative(AbstraktPersona persona) {
+        if (persona == null)
+            return EMPTY;
+
+        int initative = persona.getInitative();
+        int initativWuerfel = persona.getInitativWuerfel();
+        Object[] arr = new Object[]{ initative, initativWuerfel };
+        return Shr5ManagementEditPlugin.INSTANCE.getString("_UI_Initative_Format_String", arr, true);
+    }
+
+    /**
+     * Really simple escape function.
+     * 
+     * @param s
+     * @return
+     */
+    public static String escapeXML(String s) {
+        StringBuilder result = new StringBuilder();
+        StringCharacterIterator i = new StringCharacterIterator(s);
+        char c = i.current();
+        while (c != CharacterIterator.DONE) {
+            switch (c) {
+                case '<':
+                    result.append("&lt;");
+                    break;
+
+                case '>':
+                    result.append("&gt;");
+
+                case '"':
+                    result.append("&quot;");
+
+                case '\'':
+                    result.append("&apos;");
+
+                case '&':
+                    result.append("&amp;");
+
+                default:
+                    result.append(c);
+            }
+            c = i.next();
+        }
+        return result.toString();
     }
 }
