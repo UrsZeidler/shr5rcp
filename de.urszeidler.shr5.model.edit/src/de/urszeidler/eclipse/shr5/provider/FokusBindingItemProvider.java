94048fd2d3be49c27a95baa7bade4929cf2715db
UrsZeidler <github@urszeidler.de>
2014-05-25 12:02:21 +0200

    OPEN - task 48: Support foci binding http://github.com/UrsZeidler/shr5rcp/issues/issue/48 reduce the choice of values to the items of the character

--- de.urszeidler.shr5.model.edit/src/de/urszeidler/eclipse/shr5/provider/FokusBindingItemProvider.java
+++ de.urszeidler.shr5.model.edit/src/de/urszeidler/eclipse/shr5/provider/FokusBindingItemProvider.java
@@ -3,18 +3,21 @@
  */
 package de.urszeidler.eclipse.shr5.provider;
 
-
+import de.urszeidler.eclipse.shr5.BaseMagischePersona;
+import de.urszeidler.eclipse.shr5.Cyberdeck;
+import de.urszeidler.eclipse.shr5.Fokus;
 import de.urszeidler.eclipse.shr5.FokusBinding;
 import de.urszeidler.eclipse.shr5.Shr5Package;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
-
 import org.eclipse.emf.common.util.ResourceLocator;
-
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
 import org.eclipse.emf.edit.provider.IItemLabelProvider;
@@ -30,20 +33,16 @@ import org.eclipse.emf.edit.provider.ViewerNotification;
  * This is the item provider adapter for a {@link de.urszeidler.eclipse.shr5.FokusBinding} object.
  * <!-- begin-user-doc -->
  * <!-- end-user-doc -->
+ * 
  * @generated
  */
-public class FokusBindingItemProvider
-    extends ItemProviderAdapter
-    implements
-        IEditingDomainItemProvider,
-        IStructuredItemContentProvider,
-        ITreeItemContentProvider,
-        IItemLabelProvider,
-        IItemPropertySource {
+public class FokusBindingItemProvider extends ItemProviderAdapter implements IEditingDomainItemProvider, IStructuredItemContentProvider,
+        ITreeItemContentProvider, IItemLabelProvider, IItemPropertySource {
     /**
      * This constructs an instance from a factory and a notifier.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
+     * 
      * @generated
      */
     public FokusBindingItemProvider(AdapterFactory adapterFactory) {
@@ -54,6 +53,7 @@ public class FokusBindingItemProvider
      * This returns the property descriptors for the adapted class.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
+     * 
      * @generated
      */
     @Override
@@ -71,50 +71,59 @@ public class FokusBindingItemProvider
      * This adds a property descriptor for the Fokus feature.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @generated
+     * 
+     * @generated not
      */
     protected void addFokusPropertyDescriptor(Object object) {
-        itemPropertyDescriptors.add
-            (createItemPropertyDescriptor
-                (((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
-                 getResourceLocator(),
-                 getString("_UI_FokusBinding_fokus_feature"),
-                 getString("_UI_PropertyDescriptor_description", "_UI_FokusBinding_fokus_feature", "_UI_FokusBinding_type"),
-                 Shr5Package.Literals.FOKUS_BINDING__FOKUS,
-                 true,
-                 false,
-                 true,
-                 null,
-                 null,
-                 null));
+        itemPropertyDescriptors.add(new ItemPropertyDescriptor// createItemPropertyDescriptor
+                (((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(), getResourceLocator(),
+                        getString("_UI_FokusBinding_fokus_feature"), getString("_UI_PropertyDescriptor_description",
+                                "_UI_FokusBinding_fokus_feature", "_UI_FokusBinding_type"), Shr5Package.Literals.FOKUS_BINDING__FOKUS, true, false,
+                        true, null, null, null) {
+                    @Override
+                    public Collection<?> getChoiceOfValues(Object object) {
+                        Collection<?> choiceOfValues = super.getChoiceOfValues(object);
+
+                        FokusBinding binding = (FokusBinding)object;
+                        EObject eContainer = binding.eContainer();
+                        if (eContainer instanceof BaseMagischePersona) {
+                            BaseMagischePersona p = (BaseMagischePersona)eContainer;
+                            EObject container = p.eContainer();
+                            List<Object> arrayList = new ArrayList<Object>(choiceOfValues.size());
+                            for (Object o : choiceOfValues) {
+                                if (o instanceof EObject) {
+                                    EObject eo = (EObject)o;
+                                    if (eo.eContainer().equals(container))
+                                        arrayList.add(o);
+                                }
+                            }
+                            return arrayList;
+                        }
+                        return Collections.EMPTY_SET;
+                    }
+
+                });
     }
 
     /**
      * This adds a property descriptor for the Active feature.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
+     * 
      * @generated
      */
     protected void addActivePropertyDescriptor(Object object) {
-        itemPropertyDescriptors.add
-            (createItemPropertyDescriptor
-                (((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
-                 getResourceLocator(),
-                 getString("_UI_FokusBinding_active_feature"),
-                 getString("_UI_PropertyDescriptor_description", "_UI_FokusBinding_active_feature", "_UI_FokusBinding_type"),
-                 Shr5Package.Literals.FOKUS_BINDING__ACTIVE,
-                 true,
-                 false,
-                 false,
-                 ItemPropertyDescriptor.BOOLEAN_VALUE_IMAGE,
-                 null,
-                 null));
+        itemPropertyDescriptors.add(createItemPropertyDescriptor(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+                getResourceLocator(), getString("_UI_FokusBinding_active_feature"),
+                getString("_UI_PropertyDescriptor_description", "_UI_FokusBinding_active_feature", "_UI_FokusBinding_type"),
+                Shr5Package.Literals.FOKUS_BINDING__ACTIVE, true, false, false, ItemPropertyDescriptor.BOOLEAN_VALUE_IMAGE, null, null));
     }
 
     /**
      * This returns FokusBinding.gif.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
+     * 
      * @generated
      */
     @Override
@@ -126,6 +135,7 @@ public class FokusBindingItemProvider
      * This returns the label text for the adapted class.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
+     * 
      * @generated
      */
     @Override
@@ -139,6 +149,7 @@ public class FokusBindingItemProvider
      * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
+     * 
      * @generated
      */
     @Override
@@ -158,6 +169,7 @@ public class FokusBindingItemProvider
      * that can be created under this object.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
+     * 
      * @generated
      */
     @Override
@@ -169,6 +181,7 @@ public class FokusBindingItemProvider
      * Return the resource locator for this item provider's resources.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
+     * 
      * @generated
      */
     @Override
