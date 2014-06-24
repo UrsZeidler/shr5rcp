adfeca9c90ac6ffc9891ddfc8a65a2824dbf049a
UrsZeidler <github@urszeidler.de>
2014-06-24 22:17:22 +0200

    OPEN - task 27: Adding Self-Update to an RCP Application. http://github.com/UrsZeidler/shr5rcp/issues/issue/27 some reordering

--- de.urszeidler.shr5.product/src/de/urszeidler/shr5/product/application/ApplicationActionBarAdvisor.java
+++ de.urszeidler.shr5.product/src/de/urszeidler/shr5/product/application/ApplicationActionBarAdvisor.java
@@ -40,13 +40,18 @@ public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
     @Override
     protected void fillMenuBar(IMenuManager menuBar) {
         // nothing todo
+        MenuManager helpMenu1 = new MenuManager("&File", "project.menu.id");
+        menuBar.add(helpMenu1);
+        
+        MenuManager helpMenu2 = new MenuManager("&Window", "windows.menu.id");
+        menuBar.add(helpMenu2);
         
         MenuManager helpMenu = new MenuManager("&Help", IWorkbenchActionConstants.M_HELP);
         helpMenu.add(new GroupMarker(IWorkbenchActionConstants.MB_ADDITIONS));
         helpMenu.add(new Separator());
-        //helpMenu.add(aboutAction);
         menuBar.add(helpMenu);
         
+       // menuBar.insertAfter("windows.menu.id", helpMenu);
     }
 
 }
