863534cb865e73990bca23ba279cfb214fef99b3
UrsZeidler <github@urszeidler.de>
2014-07-12 23:10:07 +0200

    removed todos

--- de.urszeidler.shr5.runtime.tests/src/de/urszeidler/eclipse/shr5/runtime/tests/ExtendetDataAwareTest.java
+++ de.urszeidler.shr5.runtime.tests/src/de/urszeidler/eclipse/shr5/runtime/tests/ExtendetDataAwareTest.java
@@ -13,16 +13,25 @@ import junit.framework.TestCase;
  * <p>
  * The following operations are tested:
  * <ul>
- *   <li>{@link de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#addToExtendetData(de.urszeidler.eclipse.shr5.runtime.ExtendetData, java.lang.Object) <em>Add To Extendet Data</em>}</li>
- *   <li>{@link de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#getIntegerValue(de.urszeidler.eclipse.shr5.runtime.ExtendetData) <em>Get Integer Value</em>}</li>
- *   <li>{@link de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#increaseValue(de.urszeidler.eclipse.shr5.runtime.ExtendetData, int) <em>Increase Value</em>}</li>
- *   <li>{@link de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#decreaseValue(de.urszeidler.eclipse.shr5.runtime.ExtendetData, int) <em>Decrease Value</em>}</li>
- *   <li>{@link de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#hasValue(de.urszeidler.eclipse.shr5.runtime.ExtendetData) <em>Has Value</em>}</li>
- *   <li>{@link de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#addToList(de.urszeidler.eclipse.shr5.runtime.ExtendetData, java.lang.Object) <em>Add To List</em>}</li>
- *   <li>{@link de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#addUniqueToList(de.urszeidler.eclipse.shr5.runtime.ExtendetData, java.lang.Object) <em>Add Unique To List</em>}</li>
- *   <li>{@link de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#getListValue(de.urszeidler.eclipse.shr5.runtime.ExtendetData) <em>Get List Value</em>}</li>
+ * <li>
+ * {@link de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#addToExtendetData(de.urszeidler.eclipse.shr5.runtime.ExtendetData, java.lang.Object)
+ * <em>Add To Extendet Data</em>}</li>
+ * <li>{@link de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#getIntegerValue(de.urszeidler.eclipse.shr5.runtime.ExtendetData) <em>Get Integer
+ * Value</em>}</li>
+ * <li>{@link de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#increaseValue(de.urszeidler.eclipse.shr5.runtime.ExtendetData, int) <em>Increase
+ * Value</em>}</li>
+ * <li>{@link de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#decreaseValue(de.urszeidler.eclipse.shr5.runtime.ExtendetData, int) <em>Decrease
+ * Value</em>}</li>
+ * <li>{@link de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#hasValue(de.urszeidler.eclipse.shr5.runtime.ExtendetData) <em>Has Value</em>}</li>
+ * <li>{@link de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#addToList(de.urszeidler.eclipse.shr5.runtime.ExtendetData, java.lang.Object) <em>
+ * Add To List</em>}</li>
+ * <li>{@link de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#addUniqueToList(de.urszeidler.eclipse.shr5.runtime.ExtendetData, java.lang.Object)
+ * <em>Add Unique To List</em>}</li>
+ * <li>{@link de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#getListValue(de.urszeidler.eclipse.shr5.runtime.ExtendetData) <em>Get List Value
+ * </em>}</li>
  * </ul>
  * </p>
+ * 
  * @generated
  */
 public abstract class ExtendetDataAwareTest extends TestCase {
@@ -31,6 +40,7 @@ public abstract class ExtendetDataAwareTest extends TestCase {
      * The fixture for this Extendet Data Aware test case.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
+     * 
      * @generated
      */
     protected ExtendetDataAware fixture = null;
@@ -39,6 +49,7 @@ public abstract class ExtendetDataAwareTest extends TestCase {
      * Constructs a new Extendet Data Aware test case with the given name.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
+     * 
      * @generated
      */
     public ExtendetDataAwareTest(String name) {
@@ -49,6 +60,7 @@ public abstract class ExtendetDataAwareTest extends TestCase {
      * Sets the fixture for this Extendet Data Aware test case.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
+     * 
      * @generated
      */
     protected void setFixture(ExtendetDataAware fixture) {
@@ -59,6 +71,7 @@ public abstract class ExtendetDataAwareTest extends TestCase {
      * Returns the fixture for this Extendet Data Aware test case.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
+     * 
      * @generated
      */
     protected ExtendetDataAware getFixture() {
@@ -66,107 +79,110 @@ public abstract class ExtendetDataAwareTest extends TestCase {
     }
 
     /**
-     * Tests the '{@link de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#addToExtendetData(de.urszeidler.eclipse.shr5.runtime.ExtendetData, java.lang.Object) <em>Add To Extendet Data</em>}' operation.
+     * Tests the '
+     * {@link de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#addToExtendetData(de.urszeidler.eclipse.shr5.runtime.ExtendetData, java.lang.Object)
+     * <em>Add To Extendet Data</em>}' operation.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
+     * 
      * @see de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#addToExtendetData(de.urszeidler.eclipse.shr5.runtime.ExtendetData, java.lang.Object)
-     * @generated
+     * @generated not
      */
     public void testAddToExtendetData__ExtendetData_Object() {
-        // TODO: implement this operation test method
-        // Ensure that you remove @generated or mark it @generated NOT
         fail();
     }
 
     /**
-     * Tests the '{@link de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#getIntegerValue(de.urszeidler.eclipse.shr5.runtime.ExtendetData) <em>Get Integer Value</em>}' operation.
+     * Tests the '{@link de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#getIntegerValue(de.urszeidler.eclipse.shr5.runtime.ExtendetData)
+     * <em>Get Integer Value</em>}' operation.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
+     * 
      * @see de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#getIntegerValue(de.urszeidler.eclipse.shr5.runtime.ExtendetData)
-     * @generated
+     * @generated not
      */
     public void testGetIntegerValue__ExtendetData() {
-        // TODO: implement this operation test method
-        // Ensure that you remove @generated or mark it @generated NOT
         fail();
     }
 
     /**
-     * Tests the '{@link de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#increaseValue(de.urszeidler.eclipse.shr5.runtime.ExtendetData, int) <em>Increase Value</em>}' operation.
+     * Tests the '{@link de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#increaseValue(de.urszeidler.eclipse.shr5.runtime.ExtendetData, int)
+     * <em>Increase Value</em>}' operation.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
+     * 
      * @see de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#increaseValue(de.urszeidler.eclipse.shr5.runtime.ExtendetData, int)
-     * @generated
+     * @generated not
      */
     public void testIncreaseValue__ExtendetData_int() {
-        // TODO: implement this operation test method
-        // Ensure that you remove @generated or mark it @generated NOT
         fail();
     }
 
     /**
-     * Tests the '{@link de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#decreaseValue(de.urszeidler.eclipse.shr5.runtime.ExtendetData, int) <em>Decrease Value</em>}' operation.
+     * Tests the '{@link de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#decreaseValue(de.urszeidler.eclipse.shr5.runtime.ExtendetData, int)
+     * <em>Decrease Value</em>}' operation.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
+     * 
      * @see de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#decreaseValue(de.urszeidler.eclipse.shr5.runtime.ExtendetData, int)
-     * @generated
+     * @generated not
      */
     public void testDecreaseValue__ExtendetData_int() {
-        // TODO: implement this operation test method
-        // Ensure that you remove @generated or mark it @generated NOT
         fail();
     }
 
     /**
-     * Tests the '{@link de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#hasValue(de.urszeidler.eclipse.shr5.runtime.ExtendetData) <em>Has Value</em>}' operation.
+     * Tests the '{@link de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#hasValue(de.urszeidler.eclipse.shr5.runtime.ExtendetData)
+     * <em>Has Value</em>}' operation.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
+     * 
      * @see de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#hasValue(de.urszeidler.eclipse.shr5.runtime.ExtendetData)
-     * @generated
+     * @generated not
      */
     public void testHasValue__ExtendetData() {
-        // TODO: implement this operation test method
-        // Ensure that you remove @generated or mark it @generated NOT
         fail();
     }
 
     /**
-     * Tests the '{@link de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#addToList(de.urszeidler.eclipse.shr5.runtime.ExtendetData, java.lang.Object) <em>Add To List</em>}' operation.
+     * Tests the '
+     * {@link de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#addToList(de.urszeidler.eclipse.shr5.runtime.ExtendetData, java.lang.Object)
+     * <em>Add To List</em>}' operation.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
+     * 
      * @see de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#addToList(de.urszeidler.eclipse.shr5.runtime.ExtendetData, java.lang.Object)
-     * @generated
+     * @generated not
      */
     public void testAddToList__ExtendetData_Object() {
-        // TODO: implement this operation test method
-        // Ensure that you remove @generated or mark it @generated NOT
         fail();
     }
 
     /**
-     * Tests the '{@link de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#addUniqueToList(de.urszeidler.eclipse.shr5.runtime.ExtendetData, java.lang.Object) <em>Add Unique To List</em>}' operation.
+     * Tests the '
+     * {@link de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#addUniqueToList(de.urszeidler.eclipse.shr5.runtime.ExtendetData, java.lang.Object)
+     * <em>Add Unique To List</em>}' operation.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
+     * 
      * @see de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#addUniqueToList(de.urszeidler.eclipse.shr5.runtime.ExtendetData, java.lang.Object)
-     * @generated
+     * @generated not
      */
     public void testAddUniqueToList__ExtendetData_Object() {
-        // TODO: implement this operation test method
-        // Ensure that you remove @generated or mark it @generated NOT
         fail();
     }
 
     /**
-     * Tests the '{@link de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#getListValue(de.urszeidler.eclipse.shr5.runtime.ExtendetData) <em>Get List Value</em>}' operation.
+     * Tests the '{@link de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#getListValue(de.urszeidler.eclipse.shr5.runtime.ExtendetData)
+     * <em>Get List Value</em>}' operation.
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
+     * 
      * @see de.urszeidler.eclipse.shr5.runtime.ExtendetDataAware#getListValue(de.urszeidler.eclipse.shr5.runtime.ExtendetData)
-     * @generated
+     * @generated not
      */
     public void testGetListValue__ExtendetData() {
-        // TODO: implement this operation test method
-        // Ensure that you remove @generated or mark it @generated NOT
         fail();
     }
 
-} //ExtendetDataAwareTest
+} // ExtendetDataAwareTest
