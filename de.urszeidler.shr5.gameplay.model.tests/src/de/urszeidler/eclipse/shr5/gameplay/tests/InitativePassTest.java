4302805f5f21ba6c0804d393c1adef0772eb69e4
UrsZeidler <github@urszeidler.de>
2014-07-23 21:28:45 +0200

    added tests

--- de.urszeidler.shr5.gameplay.model.tests/src/de/urszeidler/eclipse/shr5/gameplay/tests/InitativePassTest.java
+++ de.urszeidler.shr5.gameplay.model.tests/src/de/urszeidler/eclipse/shr5/gameplay/tests/InitativePassTest.java
@@ -3,9 +3,13 @@
  */
 package de.urszeidler.eclipse.shr5.gameplay.tests;
 
+import de.urszeidler.eclipse.shr5.gameplay.ComplexAction;
+import de.urszeidler.eclipse.shr5.gameplay.FreeAction;
 import de.urszeidler.eclipse.shr5.gameplay.GameplayFactory;
 import de.urszeidler.eclipse.shr5.gameplay.InitativePass;
-
+import de.urszeidler.eclipse.shr5.gameplay.SimpleActions;
+import de.urszeidler.eclipse.shr5.gameplay.util.GameplayTools;
+import de.urszeidler.eclipse.shr5.runtime.RuntimeCharacter;
 import junit.textui.TestRunner;
 
 /**
@@ -50,11 +54,14 @@ public class InitativePassTest extends SubjectCommandTest {
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
      * @see junit.framework.TestCase#setUp()
-     * @generated
+     * @generated not 
      */
     @Override
     protected void setUp() throws Exception {
         setFixture(GameplayFactory.eINSTANCE.createInitativePass());
+        RuntimeCharacter character1 = GameplayTools.createRuntimeCharacter();
+        getFixture().setSubject(character1);
+
     }
 
     /**
@@ -68,4 +75,77 @@ public class InitativePassTest extends SubjectCommandTest {
         setFixture(null);
     }
 
+    /**
+     * Tests the '{@link de.urszeidler.eclipse.shr5.gameplay.Command#isCanExecute() <em>Can Execute</em>}' feature getter.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @see de.urszeidler.eclipse.shr5.gameplay.Command#isCanExecute()
+     * @generated not
+     */
+    public void testIsCanExecute() {
+        assertTrue(getFixture().isCanExecute());
+        getFixture().setSubject(null);
+        assertFalse(getFixture().isCanExecute());
+    }
+
+    /**
+     * Tests the '{@link de.urszeidler.eclipse.shr5.gameplay.Command#redo() <em>Redo</em>}' operation.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @see de.urszeidler.eclipse.shr5.gameplay.Command#redo()
+     * @generated not
+     */
+    public void testRedo() {
+        assertFalse(getFixture().isExecuted());
+        FreeAction freeAction = GameplayFactory.eINSTANCE.createFreeAction();
+        getFixture().setFreeAction(freeAction);
+        getFixture().redo();
+        assertTrue(getFixture().isExecuted());
+        assertTrue(freeAction.isExecuted());
+    }
+
+    /**
+     * Tests the '{@link de.urszeidler.eclipse.shr5.gameplay.Command#redo() <em>Redo</em>}' operation.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @see de.urszeidler.eclipse.shr5.gameplay.Command#redo()
+     * @generated not
+     */
+    public void testRedo_complexAction() {
+        assertFalse(getFixture().isExecuted());
+        ComplexAction complexAction = GameplayFactory.eINSTANCE.createComplexAction();
+        getFixture().setAction(complexAction);
+        getFixture().redo();
+        assertTrue(getFixture().isExecuted());
+        assertTrue(complexAction.isExecuted());
+    }
+
+    /**
+     * Tests the '{@link de.urszeidler.eclipse.shr5.gameplay.Command#redo() <em>Redo</em>}' operation.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @see de.urszeidler.eclipse.shr5.gameplay.Command#redo()
+     * @generated not
+     */
+    public void testRedo_simpleAction() {
+        assertFalse(getFixture().isExecuted());
+        SimpleActions complexAction = GameplayFactory.eINSTANCE.createSimpleActions();
+        getFixture().setAction(complexAction);
+        getFixture().redo();
+        assertTrue(getFixture().isExecuted());
+        assertTrue(complexAction.isExecuted());
+    }
+
+    
+    /**
+     * Tests the '{@link de.urszeidler.eclipse.shr5.gameplay.Command#undo() <em>Undo</em>}' operation.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @see de.urszeidler.eclipse.shr5.gameplay.Command#undo()
+     * @generated not
+     */
+    public void testUndo() {
+        //fail();
+    }
+    
 } //InitativePassTest
