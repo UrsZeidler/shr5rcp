6bc9827af798e1e5838dbf5541dae9b23ab2e473
UrsZeidler <github@urszeidler.de>
2014-07-31 22:23:28 +0200

    added protocoll

---
+++ de.urszeidler.shr5.ecp/src/de/urszeidler/shr5/ecp/service/ScriptViewer.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package de.urszeidler.shr5.ecp.service;
+
+import de.urszeidler.shr5.scripting.Placement;
+import de.urszeidler.shr5.scripting.Script;
+
+/**
+ * @author urs
+ *
+ */
+public interface ScriptViewer {
+
+    void setPlacement(Placement placement);
+
+    void setScript(Script script);
+
+}
