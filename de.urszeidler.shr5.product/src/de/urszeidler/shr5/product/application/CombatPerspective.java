20e19be94a48a8368c432ad3f4ed99b5bc420418
UrsZeidler <github@urszeidler.de>
2014-08-09 13:15:19 +0200

    added combat perspective

--- de.urszeidler.shr5.product/src/de/urszeidler/shr5/product/application/CombatPerspective.java
+++ de.urszeidler.shr5.product/src/de/urszeidler/shr5/product/application/CombatPerspective.java
@@ -2,9 +2,8 @@ package de.urszeidler.shr5.product.application;
 
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
-import org.eclipse.ui.IFolderLayout;
 
-public class RuntimePerspective implements IPerspectiveFactory {
+public class CombatPerspective implements IPerspectiveFactory {
 
     /**
      * Creates the initial layout for a page.
@@ -12,21 +11,19 @@ public class RuntimePerspective implements IPerspectiveFactory {
     public void createInitialLayout(IPageLayout layout) {
         layout.setFixed(true);
         layout.setEditorAreaVisible(false);
-//        String editorArea = layout.getEditorArea();
+        
+        //String editorArea = layout.getEditorArea();
         addFastViews(layout);
         addViewShortcuts(layout);
         addPerspectiveShortcuts(layout);
-        {
-            IFolderLayout folderLayout = layout.createFolder("folder", IPageLayout.RIGHT, 0.5f, IPageLayout.ID_EDITOR_AREA);
-            folderLayout.addView("de.urszeidler.shr5.runtime.ui.views.RuntimeScriptView");
-        }
+        layout.addView("de.urszeidler.shr5.ecp.CombatTurnView", IPageLayout.RIGHT, 0.63f, IPageLayout.ID_EDITOR_AREA);
+        layout.addView("de.urszeidler.shr5.runtime.ui.views.RuntimeScriptView", IPageLayout.RIGHT, 0.05f, IPageLayout.ID_EDITOR_AREA);
     }
 
     /**
      * Add fast views to the perspective.
      */
     private void addFastViews(IPageLayout layout) {
-        layout.addFastView("de.urszeidler.shr5.ecp.CombatTurnView");
     }
 
     /**
