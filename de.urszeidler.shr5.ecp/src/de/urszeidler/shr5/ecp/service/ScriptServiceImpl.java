2100e0006925b57c8bd53df6ed0cd7f4ea18a1d1
UrsZeidler <github@urszeidler.de>
2014-08-11 21:46:49 +0200

    update

--- de.urszeidler.shr5.ecp/src/de/urszeidler/shr5/ecp/service/ScriptServiceImpl.java
+++ de.urszeidler.shr5.ecp/src/de/urszeidler/shr5/ecp/service/ScriptServiceImpl.java
@@ -46,6 +46,8 @@ public class ScriptServiceImpl implements ScriptService {
 
     public void setPlacement(Placement placement) {
         this.placement = placement;
+        if(script!=null)
+            script.getHistory().setCurrentPlacement(placement);
         if (scriptViewer != null)
             scriptViewer.setPlacement(placement);
     }
@@ -71,14 +73,17 @@ public class ScriptServiceImpl implements ScriptService {
         if (script != null && script.getHistory() != null && script.getHistory().getCommandStack() != null) {
             if (placement != null) {
                 Date actualDate = placement.getActualDate();
-                if (actualDate != null && command.getDate() == null)
+                if (actualDate != null && command.getDate() == null){
                     command.setDate(actualDate);
+                    script.getHistory().setCurrentDate(actualDate);   
+                }
             }
             if (scriptViewer != null && scriptViewer.getCmdCallback() != null)
                 command.setCmdCallback(scriptViewer.getCmdCallback());
 
             script.getHistory().getCommandStack().setCurrentCommand(command);
             script.getHistory().getCommandStack().redo();
+            
         }
     }
 }
