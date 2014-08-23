7bd4c1a0d4dcec385b0b4ac8b72ea4010d4819ce
UrsZeidler <github@urszeidler.de>
2014-08-23 13:27:04 +0200

    fixed the probe tests

--- de.urszeidler.shr5.gameplay.model.tests/src/de/urszeidler/eclipse/shr5/gameplay/tests/ProbeCommandTest.java
+++ de.urszeidler.shr5.gameplay.model.tests/src/de/urszeidler/eclipse/shr5/gameplay/tests/ProbeCommandTest.java
@@ -4,6 +4,8 @@
 package de.urszeidler.eclipse.shr5.gameplay.tests;
 
 import de.urszeidler.eclipse.shr5.gameplay.ProbeCommand;
+import de.urszeidler.eclipse.shr5.gameplay.ProbeState;
+import de.urszeidler.eclipse.shr5.gameplay.SuccesTestState;
 
 /**
  * <!-- begin-user-doc -->
@@ -46,12 +48,28 @@ public abstract class ProbeCommandTest extends SubjectCommandTest {
      * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
      * @see de.urszeidler.eclipse.shr5.gameplay.Probe#getProbeState()
-     * @generated
+     * @generated not
      */
     public void testGetProbeState() {
-        // TODO: implement this feature getter test method
-        // Ensure that you remove @generated or mark it @generated NOT
-        fail();
+        getFixture().getProbe().add(1);
+        getFixture().getProbe().add(1);
+        getFixture().getProbe().add(1);
+        getFixture().getProbe().add(1);
+        getFixture().setGlitches(4);
+        getFixture().setExecuted(true);
+
+        assertEquals(ProbeState.CRITICAL_GLITCH, getFixture().getProbeState());
+        
+        getFixture().getProbe().add(5);
+        getFixture().setSuccesses(1);
+        getFixture().getGlitches();
+        assertEquals(ProbeState.GLITCH, getFixture().getProbeState());
+        
+        getFixture().getProbe().add(2);
+        getFixture().getProbe().add(2);
+        getFixture().getProbe().add(2);
+        getFixture().getProbe().add(2);
+        assertEquals(ProbeState.NO_GLITCH, getFixture().getProbeState());
     }
 
     /**
@@ -62,9 +80,25 @@ public abstract class ProbeCommandTest extends SubjectCommandTest {
      * @generated
      */
     public void testGetTestState() {
-        // TODO: implement this feature getter test method
-        // Ensure that you remove @generated or mark it @generated NOT
-        fail();
+        
+        
+        getFixture().getProbe().add(1);
+        getFixture().getProbe().add(1);
+        getFixture().getProbe().add(1);
+        getFixture().getProbe().add(1);
+        getFixture().setGlitches(4);
+        getFixture().setExecuted(true);
+        getFixture().setNetHits(-2);
+        getFixture().setThresholds(2);
+        
+        assertEquals(SuccesTestState.FAILURE, getFixture().getTestState());
+        
+        getFixture().getProbe().add(5);
+        getFixture().getProbe().add(5);
+        getFixture().setSuccesses(2);
+        getFixture().setNetHits(0);
+        assertEquals(SuccesTestState.SUCCES, getFixture().getTestState());
+        
     }
 
 } //ProbeCommandTest
