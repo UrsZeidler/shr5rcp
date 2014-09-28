33d0aeac30ddd84800890eb5578f13b71cbf8484
UrsZeidler <github@urszeidler.de>
2014-09-28 10:15:17 +0200

    added some range tests

--- de.urszeidler.shr5.gameplay.model.tests/src/de/urszeidler/eclipse/shr5/gameplay/tests/RangedAttackCmdTest.java
+++ de.urszeidler.shr5.gameplay.model.tests/src/de/urszeidler/eclipse/shr5/gameplay/tests/RangedAttackCmdTest.java
@@ -9,6 +9,7 @@ import de.urszeidler.eclipse.shr5.Fertigkeit;
 import de.urszeidler.eclipse.shr5.FeuerModus;
 import de.urszeidler.eclipse.shr5.Feuerwaffe;
 import de.urszeidler.eclipse.shr5.PersonaFertigkeit;
+import de.urszeidler.eclipse.shr5.Reichweite;
 import de.urszeidler.eclipse.shr5.Shr5Factory;
 import de.urszeidler.eclipse.shr5.Shr5Package;
 import de.urszeidler.eclipse.shr5.gameplay.GameplayFactory;
@@ -159,6 +160,174 @@ public class RangedAttackCmdTest extends OpposedSkillTestCmdTest {
         getFixture().redo();
 
         assertTrue(getFixture().isExecuted());
+//        assertEquals(2, getFixture().getProbe().size());
+        
+    }
+
+    /**
+     * Tests the '{@link de.urszeidler.eclipse.shr5.gameplay.Command#redo() <em>Redo</em>}' operation.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * 
+     * @see de.urszeidler.eclipse.shr5.gameplay.Command#redo()
+     * @generated not
+     */
+    public void testRedo_skip_range_Short() {
+        RuntimeCharacter runtimeCharacter = GameplayTools.createRuntimeCharacter();
+        RuntimeCharacter object = GameplayTools.createRuntimeCharacter();
+        AbstraktPersona persona = runtimeCharacter.getCharacter().getPersona();
+        persona.setGeschicklichkeitBasis(1);
+
+        Feuerwaffe fw = Shr5Factory.eINSTANCE.createFeuerwaffe();
+        fw.getModie().add(FeuerModus.EM);
+        Reichweite reichweite = Shr5Factory.eINSTANCE.createReichweite();
+        reichweite.setMin(0);
+        reichweite.setKurz(5);
+        reichweite.setMittel(10);
+        
+        fw.setReichweite(reichweite);
+        Fertigkeit fertigkeit = Shr5Factory.eINSTANCE.createFertigkeit();
+        fertigkeit.setAttribut(Shr5Package.Literals.KOERPERLICHE_ATTRIBUTE__GESCHICKLICHKEIT);
+
+        PersonaFertigkeit personaFertigkeit = Shr5Factory.eINSTANCE.createPersonaFertigkeit();
+        personaFertigkeit.setFertigkeit(fertigkeit);
+        personaFertigkeit.setStufe(1);
+        persona.getFertigkeiten().add(personaFertigkeit);
+
+        fw.setFertigkeit(fertigkeit);
+        getFixture().setSubject(runtimeCharacter);
+        getFixture().setWeapon(fw);
+        getFixture().setObject(object);
+        getFixture().setRange(3);
+        
+        getFixture().setSkipTest(true);
+        getFixture().setSuccesses(0);
+        getFixture().redo();
+
+        assertTrue(getFixture().isExecuted());
+        assertEquals(0, getFixture().getMods());
+        assertEquals(0, getFixture().getProbeMods().size());
+//        assertEquals(2, getFixture().getProbe().size());
+        
+    }
+    
+    public void testRedo_skip_range_Med() {
+        RuntimeCharacter runtimeCharacter = GameplayTools.createRuntimeCharacter();
+        RuntimeCharacter object = GameplayTools.createRuntimeCharacter();
+        AbstraktPersona persona = runtimeCharacter.getCharacter().getPersona();
+        persona.setGeschicklichkeitBasis(1);
+
+        Feuerwaffe fw = Shr5Factory.eINSTANCE.createFeuerwaffe();
+        fw.getModie().add(FeuerModus.EM);
+        Reichweite reichweite = Shr5Factory.eINSTANCE.createReichweite();
+        reichweite.setMin(0);
+        reichweite.setKurz(5);
+        reichweite.setMittel(10);
+        
+        fw.setReichweite(reichweite);
+        Fertigkeit fertigkeit = Shr5Factory.eINSTANCE.createFertigkeit();
+        fertigkeit.setAttribut(Shr5Package.Literals.KOERPERLICHE_ATTRIBUTE__GESCHICKLICHKEIT);
+
+        PersonaFertigkeit personaFertigkeit = Shr5Factory.eINSTANCE.createPersonaFertigkeit();
+        personaFertigkeit.setFertigkeit(fertigkeit);
+        personaFertigkeit.setStufe(1);
+        persona.getFertigkeiten().add(personaFertigkeit);
+
+        fw.setFertigkeit(fertigkeit);
+        getFixture().setSubject(runtimeCharacter);
+        getFixture().setWeapon(fw);
+        getFixture().setObject(object);
+        getFixture().setRange(7);
+        
+        getFixture().setSkipTest(true);
+        getFixture().setSuccesses(0);
+        getFixture().redo();
+
+        assertTrue(getFixture().isExecuted());
+        assertEquals(-1, getFixture().getMods());
+        assertEquals(1, getFixture().getProbeMods().size());
+//        assertEquals(2, getFixture().getProbe().size());
+        
+    }
+    
+    public void testRedo_skip_range_Long() {
+        RuntimeCharacter runtimeCharacter = GameplayTools.createRuntimeCharacter();
+        RuntimeCharacter object = GameplayTools.createRuntimeCharacter();
+        AbstraktPersona persona = runtimeCharacter.getCharacter().getPersona();
+        persona.setGeschicklichkeitBasis(1);
+
+        Feuerwaffe fw = Shr5Factory.eINSTANCE.createFeuerwaffe();
+        fw.getModie().add(FeuerModus.EM);
+        Reichweite reichweite = Shr5Factory.eINSTANCE.createReichweite();
+        reichweite.setMin(0);
+        reichweite.setKurz(5);
+        reichweite.setMittel(10);
+        reichweite.setWeit(20);
+        reichweite.setExtrem(30);
+        
+        fw.setReichweite(reichweite);
+        Fertigkeit fertigkeit = Shr5Factory.eINSTANCE.createFertigkeit();
+        fertigkeit.setAttribut(Shr5Package.Literals.KOERPERLICHE_ATTRIBUTE__GESCHICKLICHKEIT);
+
+        PersonaFertigkeit personaFertigkeit = Shr5Factory.eINSTANCE.createPersonaFertigkeit();
+        personaFertigkeit.setFertigkeit(fertigkeit);
+        personaFertigkeit.setStufe(1);
+        persona.getFertigkeiten().add(personaFertigkeit);
+
+        fw.setFertigkeit(fertigkeit);
+        getFixture().setSubject(runtimeCharacter);
+        getFixture().setWeapon(fw);
+        getFixture().setObject(object);
+        getFixture().setRange(15);
+        
+        getFixture().setSkipTest(true);
+        getFixture().setSuccesses(0);
+        getFixture().redo();
+
+        assertTrue(getFixture().isExecuted());
+        assertEquals(-3, getFixture().getMods());
+        assertEquals(1, getFixture().getProbeMods().size());
+//        assertEquals(2, getFixture().getProbe().size());
+        
+    }
+
+    public void testRedo_skip_range_Extrem() {
+        RuntimeCharacter runtimeCharacter = GameplayTools.createRuntimeCharacter();
+        RuntimeCharacter object = GameplayTools.createRuntimeCharacter();
+        AbstraktPersona persona = runtimeCharacter.getCharacter().getPersona();
+        persona.setGeschicklichkeitBasis(1);
+
+        Feuerwaffe fw = Shr5Factory.eINSTANCE.createFeuerwaffe();
+        fw.getModie().add(FeuerModus.EM);
+        Reichweite reichweite = Shr5Factory.eINSTANCE.createReichweite();
+        reichweite.setMin(0);
+        reichweite.setKurz(5);
+        reichweite.setMittel(10);
+        reichweite.setWeit(20);
+        reichweite.setExtrem(30);
+        
+        fw.setReichweite(reichweite);
+        Fertigkeit fertigkeit = Shr5Factory.eINSTANCE.createFertigkeit();
+        fertigkeit.setAttribut(Shr5Package.Literals.KOERPERLICHE_ATTRIBUTE__GESCHICKLICHKEIT);
+
+        PersonaFertigkeit personaFertigkeit = Shr5Factory.eINSTANCE.createPersonaFertigkeit();
+        personaFertigkeit.setFertigkeit(fertigkeit);
+        personaFertigkeit.setStufe(1);
+        persona.getFertigkeiten().add(personaFertigkeit);
+
+        fw.setFertigkeit(fertigkeit);
+        getFixture().setSubject(runtimeCharacter);
+        getFixture().setWeapon(fw);
+        getFixture().setObject(object);
+        getFixture().setRange(25);
+        
+        getFixture().setSkipTest(true);
+        getFixture().setSuccesses(0);
+        getFixture().redo();
+
+        assertTrue(getFixture().isExecuted());
+        assertEquals(-6, getFixture().getMods());
+        assertEquals(1, getFixture().getProbeMods().size());
 //        assertEquals(2, getFixture().getProbe().size());
         
     }
