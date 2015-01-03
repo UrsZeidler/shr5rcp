0412007d36c154e6d65f2f6aefd9e98fe28b4048
UrsZeidler <github@urszeidler.de>
2015-01-03 11:20:35 +0100

    added current extendet datat state in the tooltip

--- de.urszeidler.shr5.ecp/src/de/urszeidler/shr5/runtime/ui/widgets/RuntimeCharacterTooltip.java
+++ de.urszeidler.shr5.ecp/src/de/urszeidler/shr5/runtime/ui/widgets/RuntimeCharacterTooltip.java
@@ -3,15 +3,22 @@
  */
 package de.urszeidler.shr5.runtime.ui.widgets;
 
+import java.util.Map.Entry;
+import java.util.Set;
+
 import org.eclipse.core.databinding.DataBindingContext;
 import org.eclipse.core.databinding.UpdateValueStrategy;
 import org.eclipse.core.databinding.conversion.IConverter;
 import org.eclipse.core.databinding.observable.value.IObservableValue;
 import org.eclipse.emf.databinding.EMFObservables;
 import org.eclipse.emf.databinding.EMFUpdateValueStrategy;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.action.ToolBarManager;
 import org.eclipse.jface.databinding.swt.ISWTObservableValue;
 import org.eclipse.jface.databinding.swt.IWidgetValueProperty;
 import org.eclipse.jface.databinding.swt.WidgetProperties;
+import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.window.ToolTip;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Image;
@@ -24,12 +31,15 @@ import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.ToolBar;
 
 import de.urszeidler.eclipse.shr5.AbstraktPersona;
 import de.urszeidler.eclipse.shr5.KoerperPersona;
 import de.urszeidler.eclipse.shr5.Shr5Package;
+import de.urszeidler.eclipse.shr5.runtime.ExtendetData;
 import de.urszeidler.eclipse.shr5.runtime.RuntimeCharacter;
 import de.urszeidler.eclipse.shr5.runtime.RuntimePackage;
+import de.urszeidler.eclipse.shr5.util.AdapterFactoryUtil;
 import de.urszeidler.shr5.ecp.binding.PathToImageConverter;
 
 /**
@@ -44,16 +54,17 @@ public class RuntimeCharacterTooltip extends ToolTip {
 
     private Label lblName;
     private Label label_image;
-
+    private ToolBarManager extendetDataToolbar;
     private RuntimeCharacter runtimeCharacter;
-//    private Table table;
-    
- 
+
+    // private Table table;
+
     /**
      */
     public RuntimeCharacterTooltip(Control control) {
         super(control);
         this.parentShell = control.getShell();
+
     }
 
     public RuntimeCharacterTooltip(Control control, RuntimeCharacter subject) {
@@ -70,35 +81,33 @@ public class RuntimeCharacterTooltip extends ToolTip {
      */
 
     protected Composite createToolTipContentArea(Event event, Composite parent) {
-//        Object object = event.data;
-        
-        
+        // Object object = event.data;
+
         Composite comp = new Composite(parent, SWT.NONE);
         comp.setLayout(new GridLayout(1, false));
-        
+
         lblName = new Label(comp, SWT.NONE);
-        lblName.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+        lblName.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
         lblName.setText("titel");
-        
+
         Composite composite = new Composite(comp, SWT.NONE);
         composite.setLayout(new GridLayout(4, false));
         composite.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
-        
+
         label_image = new Label(composite, SWT.NONE);
         label_image.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, false, false, 1, 1));
-        
+
         Group grpPhysical = new Group(composite, SWT.NONE);
-        grpPhysical.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, false, false, 1, 1));
+        grpPhysical.setLayoutData(new GridData(SWT.LEFT, SWT.FILL, false, false, 1, 1));
         grpPhysical.setSize(66, 82);
         grpPhysical.setText("physical");
-        
-        
+
         Group grpMental = new Group(composite, SWT.NONE);
-        grpMental.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, false, false, 1, 1));
+        grpMental.setLayoutData(new GridData(SWT.LEFT, SWT.FILL, false, false, 1, 1));
         grpMental.setText("mental");
-        
+
         Group grpRest = new Group(composite, SWT.NONE);
-        grpRest.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, false, false, 1, 1));
+        grpRest.setLayoutData(new GridData(SWT.LEFT, SWT.FILL, false, false, 1, 1));
         grpRest.setText("Extra");
         grpPhysical.setLayout(new FillLayout(SWT.HORIZONTAL));
 
@@ -106,8 +115,13 @@ public class RuntimeCharacterTooltip extends ToolTip {
         grpMental.setLayout(new FillLayout(SWT.HORIZONTAL));
         stateMonitorWidgetMental = new StateMonitorWidget(grpMental, SWT.NONE);
         grpRest.setLayout(new FillLayout(SWT.HORIZONTAL));
-        stateMonitorWidgetOverDead = new StateMonitorWidget(grpRest, SWT.NONE,3,false);
-        
+        stateMonitorWidgetOverDead = new StateMonitorWidget(grpRest, SWT.NONE, 3, false);
+
+        Group grpExtendedData = new Group(comp, SWT.NONE);
+        grpExtendedData.setLayout(new FillLayout(SWT.HORIZONTAL));
+        grpExtendedData.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, false, 1, 1));
+        grpExtendedData.setText("extended data");
+
         if (runtimeCharacter != null && runtimeCharacter.getCharacter() != null && runtimeCharacter.getCharacter().getPersona() != null) {
             AbstraktPersona persona2 = runtimeCharacter.getCharacter().getPersona();
             KoerperPersona koerperPersona = (KoerperPersona)persona2;
@@ -115,64 +129,87 @@ public class RuntimeCharacterTooltip extends ToolTip {
             stateMonitorWidgetPhysical.setMaxConditions(koerperPersona.getZustandKoerperlichMax());
             stateMonitorWidgetOverDead.setMaxConditions(koerperPersona.getZustandGrenze());
         }
+        extendetDataToolbar = new ToolBarManager(SWT.VERTICAL);
 
+        LabelProvider labelProvider = AdapterFactoryUtil.getInstance().getLabelProvider();
+
+        Set<Entry<ExtendetData, Object>> entrySet = runtimeCharacter.getExtendetData().entrySet();
+        for (Entry<ExtendetData, Object> entry : entrySet) {
+            ExtendetData key = entry.getKey();
+//            if (key.getEFeature().equals(RuntimePackage.Literals.NAHKAMP_MODIFIKATIONEN__CHARACTER_PRONE))
+//                extendetDataToolbar.add(new Action("prone") {
+//                });
+//            if (key.getEFeature().equals(Shr5Package.Literals.FEUERWAFFE__RUECKSTOSS))
+//                extendetDataToolbar.add(new Action("recoile") {
+//                });
+
+            if (key.getEObject() != null) {
+                extendetDataToolbar.add(new Action(labelProvider.getText(key.getEObject()) + " " + labelProvider.getText(key.getEFeature()) + "  "
+                        + entry.getValue()) {
+                });
+            }
+        }
+        ToolBar createControl = extendetDataToolbar.createControl(grpExtendedData);
         
-//        Composite composite_1 = new Composite(comp, SWT.NONE);
-//        composite_1.setLayout(new FillLayout(SWT.HORIZONTAL));
-//        composite_1.setLayoutData(new GridData(SWT.FILL, SWT.TOP, false, false, 1, 1));
-        
-//        TableViewer tableViewer = new TableViewer(composite_1, SWT.BORDER | SWT.FULL_SELECTION);
-//        table = tableViewer.getTable();
-//        
-//        TableViewerColumn tableViewerColumn = new TableViewerColumn(tableViewer, SWT.NONE);
-//        TableColumn tblclmnData = tableViewerColumn.getColumn();
-//        tblclmnData.setWidth(150);
-//        tblclmnData.setText("data");
-//        tableViewerColumn.setLabelProvider(new CellLabelProvider() {            
-//            @Override
-//            public void update(ViewerCell cell) {
-//               Entry<ExtendetData, Object> element = (Entry<ExtendetData,Object>)cell.getElement();
-//               cell.setText(element.getKey().toString());                
-//            }
-//        });
-//        
-//        TableViewerColumn tableViewerColumn_1 = new TableViewerColumn(tableViewer, SWT.NONE);
-//        TableColumn tblclmnValue = tableViewerColumn_1.getColumn();
-//        tblclmnValue.setWidth(100);
-//        tblclmnValue.setText("value");
-//        tableViewerColumn_1.setLabelProvider(new CellLabelProvider() {
-//            
-//            @Override
-//            public void update(ViewerCell cell) {
-//                Entry<ExtendetData, Object> element = (Entry<ExtendetData,Object>)cell.getElement();
-//                cell.setText(element.getValue().toString());
-//            }
-//        });
-//         
-//        tableViewer.setContentProvider(new  SimpleListContenProvider(new AdapterFactoryContentProvider(AdapterFactoryUtil.getInstance().getAdapterFactory())));
-//        tableViewer.setInput(runtimeCharacter.getExtendetData().entrySet());
-//        tableViewer.set
-        
+        grpExtendedData.layout(true, true);        
+
+        // Composite composite_1 = new Composite(comp, SWT.NONE);
+        // composite_1.setLayout(new FillLayout(SWT.HORIZONTAL));
+        // composite_1.setLayoutData(new GridData(SWT.FILL, SWT.TOP, false, false, 1, 1));
+
+        // TableViewer tableViewer = new TableViewer(composite_1, SWT.BORDER | SWT.FULL_SELECTION);
+        // table = tableViewer.getTable();
+        //
+        // TableViewerColumn tableViewerColumn = new TableViewerColumn(tableViewer, SWT.NONE);
+        // TableColumn tblclmnData = tableViewerColumn.getColumn();
+        // tblclmnData.setWidth(150);
+        // tblclmnData.setText("data");
+        // tableViewerColumn.setLabelProvider(new CellLabelProvider() {
+        // @Override
+        // public void update(ViewerCell cell) {
+        // Entry<ExtendetData, Object> element = (Entry<ExtendetData,Object>)cell.getElement();
+        // cell.setText(element.getKey().toString());
+        // }
+        // });
+        //
+        // TableViewerColumn tableViewerColumn_1 = new TableViewerColumn(tableViewer, SWT.NONE);
+        // TableColumn tblclmnValue = tableViewerColumn_1.getColumn();
+        // tblclmnValue.setWidth(100);
+        // tblclmnValue.setText("value");
+        // tableViewerColumn_1.setLabelProvider(new CellLabelProvider() {
+        //
+        // @Override
+        // public void update(ViewerCell cell) {
+        // Entry<ExtendetData, Object> element = (Entry<ExtendetData,Object>)cell.getElement();
+        // cell.setText(element.getValue().toString());
+        // }
+        // });
+        //
+        // tableViewer.setContentProvider(new SimpleListContenProvider(new
+        // AdapterFactoryContentProvider(AdapterFactoryUtil.getInstance().getAdapterFactory())));
+        // tableViewer.setInput(runtimeCharacter.getExtendetData().entrySet());
+        // tableViewer.set
+
         initDataBindings();
         return comp;
     }
 
-//    protected Composite createContentArea(Composite parent) {
-//        return new Composite(parent, SWT.NONE);
-//    }
+    // protected Composite createContentArea(Composite parent) {
+    // return new Composite(parent, SWT.NONE);
+    // }
 
     protected void openHelp() {
         parentShell.setFocus();
     }
-    
+
     protected DataBindingContext initDataBindings() {
         DataBindingContext bindingContext = new DataBindingContext();
         //
 
         IWidgetValueProperty image = WidgetProperties.image();
         ISWTObservableValue observedImage = image.observe(label_image);
-        IObservableValue observeValue = EMFObservables.observeValue(bindingContext.getValidationRealm(), runtimeCharacter.getCharacter().getPersona(),
-                Shr5Package.Literals.BESCHREIBBAR__IMAGE);
+        IObservableValue observeValue = EMFObservables.observeValue(bindingContext.getValidationRealm(),
+                runtimeCharacter.getCharacter().getPersona(), Shr5Package.Literals.BESCHREIBBAR__IMAGE);
 
         IConverter converter = null;
 
@@ -183,34 +220,31 @@ public class RuntimeCharacterTooltip extends ToolTip {
         //
         //
         IObservableValue observeTextLabel_nameObserveWidget_1 = WidgetProperties.text().observe(lblName);
-        IObservableValue characterNameObserveValue = EMFObservables.observeValue(bindingContext.getValidationRealm(), runtimeCharacter.getCharacter().getPersona(),
-                Shr5Package.Literals.BESCHREIBBAR__NAME);
+        IObservableValue characterNameObserveValue = EMFObservables.observeValue(bindingContext.getValidationRealm(), runtimeCharacter.getCharacter()
+                .getPersona(), Shr5Package.Literals.BESCHREIBBAR__NAME);
         bindingContext.bindValue(observeTextLabel_nameObserveWidget_1, characterNameObserveValue, new UpdateValueStrategy(
                 UpdateValueStrategy.POLICY_NEVER), new EMFUpdateValueStrategy());
         //
-        if(stateMonitorWidgetMental!=null){
-        ISWTObservableValue observe = new DamageStateValueProperty().observe(stateMonitorWidgetMental);
-        IObservableValue observeValue1 = EMFObservables.observeValue(bindingContext.getValidationRealm(), runtimeCharacter,
-                RuntimePackage.Literals.PHYICAL_STATE__MENTAL_DAMAGE);
-        bindingContext.bindValue(observe, observeValue1, new UpdateValueStrategy(
-                UpdateValueStrategy.POLICY_NEVER), new EMFUpdateValueStrategy());
+        if (stateMonitorWidgetMental != null) {
+            ISWTObservableValue observe = new DamageStateValueProperty().observe(stateMonitorWidgetMental);
+            IObservableValue observeValue1 = EMFObservables.observeValue(bindingContext.getValidationRealm(), runtimeCharacter,
+                    RuntimePackage.Literals.PHYICAL_STATE__MENTAL_DAMAGE);
+            bindingContext.bindValue(observe, observeValue1, new UpdateValueStrategy(UpdateValueStrategy.POLICY_NEVER), new EMFUpdateValueStrategy());
 
         }
-        if(stateMonitorWidgetPhysical!=null){
-        ISWTObservableValue observe = new DamageStateValueProperty().observe(stateMonitorWidgetPhysical);
-        IObservableValue observeValue1 = EMFObservables.observeValue(bindingContext.getValidationRealm(), runtimeCharacter,
-                RuntimePackage.Literals.PHYICAL_STATE__PHYSICAL_DAMAGE);
-        bindingContext.bindValue(observe, observeValue1, new UpdateValueStrategy(
-                UpdateValueStrategy.POLICY_NEVER), new EMFUpdateValueStrategy());
+        if (stateMonitorWidgetPhysical != null) {
+            ISWTObservableValue observe = new DamageStateValueProperty().observe(stateMonitorWidgetPhysical);
+            IObservableValue observeValue1 = EMFObservables.observeValue(bindingContext.getValidationRealm(), runtimeCharacter,
+                    RuntimePackage.Literals.PHYICAL_STATE__PHYSICAL_DAMAGE);
+            bindingContext.bindValue(observe, observeValue1, new UpdateValueStrategy(UpdateValueStrategy.POLICY_NEVER), new EMFUpdateValueStrategy());
         }
-        if(stateMonitorWidgetOverDead!=null){
-        ISWTObservableValue observe = new DamageStateValueProperty().observe(stateMonitorWidgetOverDead);
-        IObservableValue observeValue1 = EMFObservables.observeValue(bindingContext.getValidationRealm(), runtimeCharacter,
-                RuntimePackage.Literals.PHYICAL_STATE__OVER_DEAD);
-        bindingContext.bindValue(observe, observeValue1, new UpdateValueStrategy(
-                UpdateValueStrategy.POLICY_NEVER), new EMFUpdateValueStrategy());
-        }       
-        
+        if (stateMonitorWidgetOverDead != null) {
+            ISWTObservableValue observe = new DamageStateValueProperty().observe(stateMonitorWidgetOverDead);
+            IObservableValue observeValue1 = EMFObservables.observeValue(bindingContext.getValidationRealm(), runtimeCharacter,
+                    RuntimePackage.Literals.PHYICAL_STATE__OVER_DEAD);
+            bindingContext.bindValue(observe, observeValue1, new UpdateValueStrategy(UpdateValueStrategy.POLICY_NEVER), new EMFUpdateValueStrategy());
+        }
+
         return bindingContext;
     }
 }
