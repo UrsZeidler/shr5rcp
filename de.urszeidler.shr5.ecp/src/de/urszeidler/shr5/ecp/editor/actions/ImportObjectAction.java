a4f28809a752f2b27fc0cc7acd8be0e147338d8d
UrsZeidler <github@urszeidler.de>
2015-05-19 21:16:38 +0200

    leave the hard work to jobs

--- de.urszeidler.shr5.ecp/src/de/urszeidler/shr5/ecp/editor/actions/ImportObjectAction.java
+++ de.urszeidler.shr5.ecp/src/de/urszeidler/shr5/ecp/editor/actions/ImportObjectAction.java
@@ -11,7 +11,6 @@ import java.util.Map;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.emf.common.util.EList;
@@ -22,7 +21,6 @@ import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.ecore.xmi.XMIResource;
 import org.eclipse.emf.edit.command.AddCommand;
-import org.eclipse.emf.edit.command.RemoveCommand;
 import org.eclipse.emf.edit.command.SetCommand;
 import org.eclipse.emf.edit.domain.AdapterFactoryEditingDomain;
 import org.eclipse.emf.edit.domain.EditingDomain;
@@ -44,7 +42,6 @@ import de.urszeidler.shr5.ecp.Activator;
 public class ImportObjectAction extends Action {
     protected EObject currentEObject;
     protected Shell shell;
-    private Job job;
 
     private String filename;
     private String objectName;
@@ -73,10 +70,8 @@ public class ImportObjectAction extends Action {
             path = System.getProperty("user.dir");
 
         FileDialog fileDialog = new FileDialog(shell, SWT.OPEN);
-        // fileDialog.setFileName(objectName+".xmi");
         fileDialog.setFilterPath(path);
         fileDialog.setFilterExtensions(new String[]{ "*.xmi" });
-        // fileDialog.setFilterNames(new String[]{"shr5 files"});
 
         fileDialog.setText(titel);
         String newFile = fileDialog.open();
@@ -87,72 +82,81 @@ public class ImportObjectAction extends Action {
         filename = newFile;
         final File file = new File(filename);
         dialogSettings.put("PATH_OPEN", file.getParentFile().getAbsolutePath());
-
-        // job = new Job(getJobName()) {
-        // @Override
-        // protected IStatus run(IProgressMonitor monitor) {
-        // return doAction(monitor);
-        // }
-        // };
-        //
-        // job.setUser(true);
-        // job.schedule();
-        doAction(new NullProgressMonitor());
+        doAction();
     }
 
-    protected IStatus doAction(IProgressMonitor monitor) {
+    protected IStatus doAction() {
         Map<?, ?> options = new HashMap<Object, Object>();
         final URI uri = URI.createFileURI(filename);
         boolean imported = false;
 
-        monitor.beginTask("Import modelelement...", 100);
-        monitor.worked(10);
         final Shr5ResourceFactoryImpl resourceSet = new Shr5ResourceFactoryImpl();
         final XMIResource resource = (XMIResource)resourceSet.createResource(uri);
         final EditingDomain editingDomain = AdapterFactoryEditingDomain.getEditingDomainFor(currentEObject);
         try {
             resource.load(options);
-            monitor.worked(20);
             EObject eObjectImport = resource.getContents().get(0);
-            String id2 = resource.getID(eObjectImport);
+            final String id2 = resource.getID(eObjectImport);
             EList<EReference> allContainments = currentEObject.eClass().getEAllContainments();
-            XMIResource orgResource = (XMIResource)currentEObject.eResource();
+            final XMIResource orgResource = (XMIResource)currentEObject.eResource();
+            final EObject eObjectImport_1 = eObjectImport;
             if (orgResource.getEObject(id2) != null) {
                 MessageDialog messageDialog = new MessageDialog(shell, "Object with id exist", null, String.format(
-                        "%s already exist, you could replace the old one or change the id and import as a new or leave it by canceling", AdapterFactoryUtil.getInstance()
-                                .getLabelProvider().getText(eObjectImport)), MessageDialog.WARNING, new String[]{ "replace", "change id",
-                        "cancle" }, 1);
+                        "%s already exist, you could replace the old one or change the id and import as a new or leave it by canceling",
+                        AdapterFactoryUtil.getInstance().getLabelProvider().getText(eObjectImport)), MessageDialog.WARNING, new String[]{ "replace",
+                        "change id", "cancle" }, 1);
                 int open = messageDialog.open();
                 if (open == 0) {
-                    EObject eObject = orgResource.getEObject(id2);
-                    EObject eContainer = eObject.eContainer();
-                    EStructuralFeature eContainingFeature = eObject.eContainingFeature();
-                    if (eContainingFeature.isMany()) {
-                        final int indexOf = ((List<?>)eContainer.eGet(eContainingFeature)).indexOf(eObject);
-                        editingDomain.getCommandStack().execute(SetCommand.create(editingDomain, eContainer, eContainingFeature, eObjectImport, indexOf));
-//                        editingDomain.getCommandStack().execute(new RemoveCommand(editingDomain, eContainer, eContainingFeature, eObject));
-//                        editingDomain.getCommandStack().execute(new AddCommand(editingDomain, eContainer, eContainingFeature, eObjectImport));
-//                        EcoreUtil.delete(eObject);
-                    } else {
-                        editingDomain.getCommandStack().execute(new SetCommand(editingDomain, eContainer, eContainingFeature, eObjectImport));
-                        EcoreUtil.delete(eObject);
-                    }
+                    Job job = new Job("Replace ") {
+
+                        @Override
+                        protected IStatus run(IProgressMonitor monitor) {
+                            EObject eObject = orgResource.getEObject(id2);
+                            EObject eContainer = eObject.eContainer();
+                            EStructuralFeature eContainingFeature = eObject.eContainingFeature();
+                            if (eContainingFeature.isMany()) {
+                                final int indexOf = ((List<?>)eContainer.eGet(eContainingFeature)).indexOf(eObject);
+                                editingDomain.getCommandStack().execute(
+                                        SetCommand.create(editingDomain, eContainer, eContainingFeature, eObjectImport_1, indexOf));
+                            } else {
+                                editingDomain.getCommandStack().execute(
+                                        new SetCommand(editingDomain, eContainer, eContainingFeature, eObjectImport_1));
+                            }
+                            return Status.OK_STATUS;
+                        }
+                    };
+                    job.setUser(true);
+                    job.schedule();
                     imported = true;
                 } else if (open == 1) {
                     eObjectImport = EcoreUtil.copy(eObjectImport);
 
-                }else
+                } else
                     return Status.OK_STATUS;
 
             }
             if (!imported)
                 for (EReference eReference : allContainments) {
                     if (eReference.getEReferenceType().isInstance(eObjectImport)) {
-                        if (eReference.isMany()) {
-                            editingDomain.getCommandStack().execute(new AddCommand(editingDomain, currentEObject, eReference, eObjectImport));
-                        } else {
-                            editingDomain.getCommandStack().execute(new SetCommand(editingDomain, currentEObject, eReference, eObjectImport));
-                        }
+                        final EReference eReference_1 = eReference;
+                        final EObject eObjectImport_2 = eObjectImport;
+                        Job job = new Job("Importing ...") {
+
+                            @Override
+                            protected IStatus run(IProgressMonitor monitor) {
+
+                                if (eReference_1.isMany()) {
+                                    editingDomain.getCommandStack().execute(
+                                            new AddCommand(editingDomain, currentEObject, eReference_1, eObjectImport_2));
+                                } else {
+                                    editingDomain.getCommandStack().execute(
+                                            new SetCommand(editingDomain, currentEObject, eReference_1, eObjectImport_2));
+                                }
+                                return Status.OK_STATUS;
+                            }
+                        };
+                        job.setUser(true);
+                        job.schedule();
                         imported = true;
                         break;
                     }
@@ -161,28 +165,9 @@ public class ImportObjectAction extends Action {
                 return new Status(Status.WARNING, Activator.PLUGIN_ID, "No feature for import found.");
         } catch (final IOException e) {
             return Status.CANCEL_STATUS;
-        } finally {
-            monitor.done();
         }
         return Status.OK_STATUS;
     }
-
-    // /**
-    // * @return
-    // */
-    // private int doDialog() {
-    // Display.getDefault().asyncExec(new Runnable() {
-    // @Override
-    // public void run() {
-    // MessageDialog messageDialog = new MessageDialog(shell, "Object with id exist", null, "", MessageDialog.WARNING, new String[]{
-    // "replace", "change id", "cancle" }, 1);
-    // int open = messageDialog.open();
-    // }
-    // });
-    //
-    //
-    // return open;
-    // }
 
     protected String getJobName() {
         return String.format("importing the file %s to  %s", filename, objectName);
