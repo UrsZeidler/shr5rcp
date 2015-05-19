0d94fa1da277b800a3dec04194fa06616e65a962
UrsZeidler <github@urszeidler.de>
2015-05-19 23:41:54 +0200

    update chummer action

--- de.urszeidler.shr5.ecp/src/de/urszeidler/shr5/ecp/editor/actions/ImportChummerAction.java
+++ de.urszeidler.shr5.ecp/src/de/urszeidler/shr5/ecp/editor/actions/ImportChummerAction.java
@@ -12,6 +12,7 @@ import javax.xml.transform.Result;
 import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
@@ -49,7 +50,6 @@ import de.urszeidler.shr5.ecp.Activator;
 public class ImportChummerAction extends Action {
     protected EObject currentEObject;
     protected Shell shell;
-    private Job job;
 
     private String filename;
     private String objectName;
@@ -78,25 +78,36 @@ public class ImportChummerAction extends Action {
             path = System.getProperty("user.dir");
 
         FileDialog fileDialog = new FileDialog(shell, SWT.OPEN);
-        // fileDialog.setFileName(objectName+".xmi");
         fileDialog.setFilterPath(path);
         fileDialog.setFilterExtensions(new String[]{ "*.chum5" });
-        // fileDialog.setFilterNames(new String[]{"shr5 files"});
 
         fileDialog.setText(titel);
         String newFile = fileDialog.open();
 
         if (newFile == null)
-            return;// Status.CANCEL_STATUS;
+            return;
 
         filename = newFile;
         final File file = new File(filename);
         dialogSettings.put("PATH_OPEN_CHUMMER", file.getParentFile().getAbsolutePath());
+        Job job = new Job("Import Chummer character") {
+            @Override
+            protected IStatus run(IProgressMonitor monitor) {
+                doImport(file);
+                return Status.OK_STATUS;
+            }
+        };
+        job.setUser(true);
+        job.schedule();
+    }
 
-        // File stylesheet = new File("/home/urs/git/shr5rcp/build/xsl/ch2shr5Character.xsl");
-        File outFile;
+    /**
+     * @param file
+     * @throws TransformerFactoryConfigurationError
+     */
+    private void doImport(final File file) throws TransformerFactoryConfigurationError {
         try {
-            outFile = File.createTempFile("charcter-import", "xmi");
+            File outFile = File.createTempFile("charcter-import", "xmi");
 
             StreamSource stylesource = new StreamSource(this.getClass().getResourceAsStream("ch2shr5Character.xsl"));
             Transformer transformer;
@@ -105,7 +116,6 @@ public class ImportChummerAction extends Action {
             Source xmlInput = new StreamSource(file);
             Result xmlOutput = new StreamResult(outFile);// new DOMResult();
             transformer.transform(xmlInput, xmlOutput);
-            // doAction(new NullProgressMonitor());
 
             final URI uri = URI.createFileURI(outFile.getAbsolutePath());
             Map<Object, Object> options = new HashMap<Object, Object>();
@@ -127,97 +137,8 @@ public class ImportChummerAction extends Action {
                     break;
                 }
             }
-
-        } catch (Exception e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
+        } catch (Exception e) {
+            Activator.logError(e);
         }
-
-    }
-
-    protected IStatus doAction(IProgressMonitor monitor) {
-        Map<?, ?> options = new HashMap<Object, Object>();
-        final URI uri = URI.createFileURI(filename);
-        boolean imported = false;
-
-        monitor.beginTask("Import modelelement...", 100);
-        monitor.worked(10);
-        final Shr5ResourceFactoryImpl resourceSet = new Shr5ResourceFactoryImpl();
-        final XMIResource resource = (XMIResource)resourceSet.createResource(uri);
-        final EditingDomain editingDomain = AdapterFactoryEditingDomain.getEditingDomainFor(currentEObject);
-        try {
-            resource.load(options);
-            monitor.worked(20);
-            EObject eObjectImport = resource.getContents().get(0);
-            String id2 = resource.getID(eObjectImport);
-            EList<EReference> allContainments = currentEObject.eClass().getEAllContainments();
-            XMIResource orgResource = (XMIResource)currentEObject.eResource();
-            if (orgResource.getEObject(id2) != null) {
-                MessageDialog messageDialog = new MessageDialog(shell, "Object with id exist", null, String.format(
-                        "%s already exist, you could replace the old one or change the id and import as a new or leave it by canceling",
-                        AdapterFactoryUtil.getInstance().getLabelProvider().getText(eObjectImport)), MessageDialog.WARNING, new String[]{ "replace",
-                        "change id", "cancle" }, 1);
-                int open = messageDialog.open();
-                if (open == 0) {
-                    EObject eObject = orgResource.getEObject(id2);
-                    EObject eContainer = eObject.eContainer();
-                    EStructuralFeature eContainingFeature = eObject.eContainingFeature();
-                    if (eContainingFeature.isMany()) {
-                        editingDomain.getCommandStack().execute(new RemoveCommand(editingDomain, eContainer, eContainingFeature, eObject));
-                        editingDomain.getCommandStack().execute(new AddCommand(editingDomain, eContainer, eContainingFeature, eObjectImport));
-                        EcoreUtil.delete(eObject);
-                    } else {
-                        editingDomain.getCommandStack().execute(new SetCommand(editingDomain, eContainer, eContainingFeature, eObjectImport));
-                        EcoreUtil.delete(eObject);
-                    }
-                    imported = true;
-                } else if (open == 1) {
-                    eObjectImport = EcoreUtil.copy(eObjectImport);
-
-                } else
-                    return Status.OK_STATUS;
-
-            }
-            if (!imported)
-                for (EReference eReference : allContainments) {
-                    if (eReference.getEReferenceType().isInstance(eObjectImport)) {
-                        if (eReference.isMany()) {
-                            editingDomain.getCommandStack().execute(new AddCommand(editingDomain, currentEObject, eReference, eObjectImport));
-                        } else {
-                            editingDomain.getCommandStack().execute(new SetCommand(editingDomain, currentEObject, eReference, eObjectImport));
-                        }
-                        imported = true;
-                        break;
-                    }
-                }
-            if (!imported)
-                return new Status(Status.WARNING, Activator.PLUGIN_ID, "No feature for import found.");
-        } catch (final IOException e) {
-            return Status.CANCEL_STATUS;
-        } finally {
-            monitor.done();
-        }
-        return Status.OK_STATUS;
-    }
-
-    // /**
-    // * @return
-    // */
-    // private int doDialog() {
-    // Display.getDefault().asyncExec(new Runnable() {
-    // @Override
-    // public void run() {
-    // MessageDialog messageDialog = new MessageDialog(shell, "Object with id exist", null, "", MessageDialog.WARNING, new String[]{
-    // "replace", "change id", "cancle" }, 1);
-    // int open = messageDialog.open();
-    // }
-    // });
-    //
-    //
-    // return open;
-    // }
-
-    protected String getJobName() {
-        return String.format("importing the file %s to  %s", filename, objectName);
     }
 }
